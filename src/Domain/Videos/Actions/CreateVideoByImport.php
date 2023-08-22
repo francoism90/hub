eb1e0455d4334856965e557f7f1891d761bfad2d
francoism90 <francoism90@users.noreply.github.com>
2023-08-22 18:08:30 +0200

    wip

--- src/Domain/Videos/Actions/CreateVideoByImport.php
+++ src/Domain/Videos/Actions/CreateVideoByImport.php
@@ -3,6 +3,7 @@
 namespace Domain\Videos\Actions;
 
 use Domain\Imports\Models\Import;
+use Domain\Imports\States\Finished;
 use Domain\Videos\Jobs\OptimizeVideo;
 use Domain\Videos\Jobs\ProcessVideo;
 use Domain\Videos\Jobs\ReleaseVideo;
@@ -16,15 +17,17 @@ class CreateVideoByImport
     public function execute(Import $import): void
     {
         DB::transaction(function () use ($import) {
-            // Create record
             $model = Video::create(
                 Arr::only($import->toArray(), ['name', 'user_id'])
             );
 
-            // Attach media
             $model
                 ->addMediaFromDisk($import->file_name, 'import')
                 ->toMediaCollection('clips');
+
+            if ($import->state->canTransitionTo(Finished::class)) {
+                $import->state->transitionTo(Finished::class);
+            }
 
             // Process model
             Bus::chain([
