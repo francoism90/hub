b23f68ee09f0e2bb99cc833b004a481558b43ff9
francoism90 <francoism90@users.noreply.github.com>
2023-08-25 19:18:06 +0200

    wip

--- src/App/Web/Videos/Views/filters.blade.php
+++ src/App/Web/Videos/Views/filters.blade.php
@@ -1,11 +1,11 @@
-<div class="bg-gray-900/70 shadow-md rounded min-w-[280px] max-w-[280px] p-6">
+<div class="bg-gray-900/70 shadow-md rounded min-w-[18rem] max-w-[18rem] p-6">
     <h3 class="headline">{{ __('By Tag') }}</h3>
 
     <div class="overflow-auto max-h-[32rem]">
-    <div class="flex flex-col flex-wrap space-y-4 p-4">
-        @foreach ($this->tags as $tag)
-            <a class="uppercase text-sm font-medium text-gray-400 hover:text-primary-500" href="#">{{ $tag->name }}</a>
-        @endforeach
-    </div>
+        <div class="flex flex-col flex-wrap space-y-4 p-4">
+            @foreach ($this->tags as $tag)
+                <a class="uppercase text-sm font-medium text-gray-400 hover:text-primary-500" href="#">{{ $tag->name }}</a>
+            @endforeach
+        </div>
     </div>
 </div>
