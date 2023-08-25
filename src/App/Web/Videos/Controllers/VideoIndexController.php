ab24820952752dd3385d44e34e594bfb2c4b3702
francoism90 <francoism90@users.noreply.github.com>
2023-08-25 16:04:31 +0200

    wip

--- src/App/Web/Videos/Controllers/VideoIndexController.php
+++ src/App/Web/Videos/Controllers/VideoIndexController.php
@@ -2,10 +2,16 @@
 
 namespace App\Web\Videos\Controllers;
 
+use Domain\Videos\Models\Video;
 use Livewire\Component;
 
 class VideoIndexController extends Component
 {
+    public function boot()
+    {
+        $this->authorize('viewAny', Video::class);
+    }
+
     public function render()
     {
         return view('videos::index');
