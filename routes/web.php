2e8d9169694d94f431fd008c267cecb8746e85c7
francoism90 <francoism90@users.noreply.github.com>
2023-08-25 16:04:57 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Web\Videos\Controllers\VideoIndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', VideoIndexController::class);
+Route::get('/', VideoIndexController::class)->middleware('auth');
 
 // Route::fallback(fn () => abort(404));
