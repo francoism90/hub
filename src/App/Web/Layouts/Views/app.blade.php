537a7c7be2c9341acdf5383a61096a1f203110c2
francoism90 <francoism90@users.noreply.github.com>
2023-08-25 15:58:33 +0200

    wip

--- src/App/Web/Layouts/Views/app.blade.php
+++ src/App/Web/Layouts/Views/app.blade.php
@@ -1,3 +1,21 @@
-<div>
-    app
-</div>
+<!doctype html>
+<html data-theme="hub" class="scroll-smooth" lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#39336c" />
+    <meta name="msapplication-TileColor" content="#39336c" />
+    <meta name="theme-color" content="#39336c" />
+    <title>Hub</title>
+    <meta name="description" content="Hub App" />
+</head>
+
+<body>
+
+{{ $slot }}
+
+</body>
+</html>
