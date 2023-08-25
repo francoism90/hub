4511c0b773d56cc07b4473590316e03bb89baf69
francoism90 <francoism90@users.noreply.github.com>
2023-08-25 16:08:10 +0200

    wip

--- src/App/Web/Videos/Controllers/VideoIndexController.php
+++ src/App/Web/Videos/Controllers/VideoIndexController.php
@@ -3,16 +3,17 @@
 namespace App\Web\Videos\Controllers;
 
 use Domain\Videos\Models\Video;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class VideoIndexController extends Component
 {
-    public function boot()
+    public function boot(): void
     {
         $this->authorize('viewAny', Video::class);
     }
 
-    public function render()
+    public function render(): View
     {
         return view('videos::index');
     }
--- src/App/Web/Videos/Views/index.blade.php
+++ src/App/Web/Videos/Views/index.blade.php
@@ -1,3 +1,3 @@
 <div>
-    foo
+    <x-layouts::navigation />
 </div>
