204b7f6119c8c9eddac0253e6062535c317649a0
francoism90 <francoism90@users.noreply.github.com>
2023-08-26 16:32:51 +0200

    wip

--- src/App/Web/Videos/Views/search.blade.php
+++ src/App/Web/Videos/Views/search.blade.php
@@ -4,10 +4,21 @@
         class="h-6 w-6" />
 
     <x-slot:content>
-        <div class="m-5 w-full max-w-3xl rounded bg-gray-900/70 p-6 shadow-md sm:m-10">
-            Modal contents...
+        <div class="m-5 w-full max-w-3xl rounded bg-gray-800 shadow-md sm:m-10">
+            <header class="flex items-center space-x-4 p-4 relative">
+                <x-heroicon-o-magnifying-glass class="h-6 w-6 text-gray-300" />
 
-            <button @click="open = false">Close Dialog</button>
+                <input
+                    class="bg-transparent border-transparent focus:outline-none w-full"
+                    placeholder="{{ __('Search') }}"
+                >
+
+                <button
+                    class="rounded-sm bg-gray-600 py-1 px-2 text-xs text-gray-200"
+                    @click="open = false">
+                        {{ __('ESC') }}
+                </button>
+            </header>
         </div>
     </x-slot:content>
 </x-layouts::dialog>
