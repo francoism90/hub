c64e68073cae5f8a6528cf1656955d0269db2a0b
francoism90 <francoism90@users.noreply.github.com>
2023-08-26 10:17:03 +0200

    wip

--- src/App/Web/Videos/Views/index.blade.php
+++ src/App/Web/Videos/Views/index.blade.php
@@ -9,11 +9,29 @@
                 @foreach ($items as $item)
                     <x-videos::item :item="$item" />
                 @endforeach
+
+                @if ($items->hasPages())
+                    <nav role="navigation" aria-label="Pagination Navigation">
+                        <span>
+                            @if ($items->onFirstPage())
+                                <span>Previous</span>
+                            @else
+                                <button wire:click="previousPage" wire:loading.attr="disabled" rel="prev">Previous</button>
+                            @endif
+                        </span>
+
+                        <span>
+                            @if ($items->onLastPage())
+                                <span>Next</span>
+                            @else
+                                <button wire:click="nextPage" wire:loading.attr="disabled" rel="next">Next</button>
+                            @endif
+                        </span>
+                    </nav>
+                @endif
             </div>
 
             <livewire:videos-filter />
-
-            {{-- {{ $items->links() }} --}}
         </main>
     </x-layouts::container>
 </div>
