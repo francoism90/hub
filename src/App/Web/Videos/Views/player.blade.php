d60e137c78bd3a625c4f90239826b738c671ed37
francoism90 <francoism90@users.noreply.github.com>
2023-08-26 10:08:32 +0200

    wip

--- src/App/Web/Videos/Views/player.blade.php
+++ src/App/Web/Videos/Views/player.blade.php
@@ -9,7 +9,82 @@
                 .registerRequestFilter(async (type, request) => (request.allowCrossSiteCredentials = true))
 
             @if ($controls)
+                // Create controls
+                const replay = (video, step) => {
+                    const el = document.createElement('button')
+
+                    el.classList.add('material-icons-round', 'shaka-tooltip')
+                    el.textContent = 'replay_10'
+
+                    el.addEventListener('click', () => {
+                        if (!video?.duration || video?.duration < step) {
+                            return
+                        }
+
+                        video.currentTime - step < video.duration
+                            ? (video.currentTime -= step)
+                            : (video.currentTime = video.duration - step)
+                    })
+
+                    return el
+                }
+
+                const forward = (video, step) => {
+                    const el = document.createElement('button')
+
+                    el.classList.add('material-icons-round', 'shaka-tooltip')
+                    el.textContent = 'forward_10'
+
+                    el.addEventListener('click', () => {
+                        if (!video?.duration || video?.duration < step) {
+                            return
+                        }
+
+                        video.currentTime + step < video.duration
+                            ? (video.currentTime += step)
+                            : (video.currentTime = video.duration - step)
+                    })
+
+                    return el
+                }
+
+                // Setup UI
                 const ui = new shaka.ui.Overlay(player, $refs.container, $refs.video)
+
+                ui.replay = class extends window.shaka.ui.Element {
+                    constructor(parent, controls) {
+                        super(parent, controls)
+                        parent.appendChild(replay($refs.video, 10))
+                    }
+                }
+
+                ui.replay.Factory = class {
+                    create(rootElement, controls) {
+                        return new ui.replay(rootElement, controls)
+                    }
+                }
+
+                window.shaka.ui.Controls.registerElement('replay', new ui.replay.Factory())
+
+                // Configure UI
+                ui.configure({
+                    addBigPlayButton: false,
+                    singleClickForPlayAndPause: false,
+                    keyboardSeekDistance: 10,
+                    controlPanelElements: [
+                        'play_pause',
+                        'replay',
+                        'time_and_duration',
+                        'spacer',
+                        'fullscreen',
+                        'overflow_menu'
+                    ],
+                    seekBarColors: {
+                        base: 'rgba(255, 255, 255, 0.3)',
+                        buffered: 'rgba(255, 255, 255, 0.54)',
+                        played: 'rgba(57, 51, 108, 1)'
+                    }
+                })
             @endif
 
             player.load('{{ $manifest }}')
