6c1a17e16ec130310a1d2a4bf99464bb89467123
francoism90 <francoism90@users.noreply.github.com>
2023-08-26 18:24:17 +0200

    wip

--- src/App/Web/Videos/Views/filters.blade.php
+++ src/App/Web/Videos/Views/filters.blade.php
@@ -1,20 +1,22 @@
-<div class="min-w-[18rem] max-w-[18rem] self-start overflow-auto">
-    <div class="rounded bg-gray-900/70 p-6 shadow-md">
-        <div class="flex cursor-pointer items-center justify-between pb-4" wire:click="toggle">
-            <h3 class="headline">{{ __('By :name', ['name' => $this->name]) }}</h3>
-            <x-heroicon-o-chevron-double-right class="h-5 w-5 text-gray-400" />
-        </div>
+<aside class="hidden md:inline-flex">
+    <div class="min-w-[18rem] max-w-[18rem] self-start overflow-auto">
+        <div class="rounded bg-gray-900/70 p-6 shadow-md">
+            <div class="flex cursor-pointer items-center justify-between pb-4" wire:click="toggle">
+                <h3 class="headline">{{ __('By :name', ['name' => $this->name]) }}</h3>
+                <x-heroicon-o-chevron-double-right class="h-5 w-5 text-gray-400" />
+            </div>
 
-        <div class="max-h-[32rem] overflow-auto px-4">
-            <div class="flex flex-col flex-wrap space-y-4">
-                @foreach ($this->tags as $item)
-                    <a
-                        class="{{ $item->getRouteKey() === $tag ? 'text-primary-500' : '' }} cursor-pointer text-sm font-medium uppercase text-gray-400 hover:text-primary-500"
-                        wire:click="$parent.setTag('{{ $item->getRouteKey() }}')">
-                        {{ $item->name }}
-                    </a>
-                @endforeach
+            <div class="max-h-[32rem] overflow-auto px-4">
+                <div class="flex flex-col flex-wrap space-y-4">
+                    @foreach ($this->tags as $item)
+                        <a
+                            class="{{ $item->getRouteKey() === $tag ? 'text-primary-500' : '' }} cursor-pointer text-sm font-medium uppercase text-gray-400 hover:text-primary-500"
+                            wire:click="$parent.setTag('{{ $item->getRouteKey() }}')">
+                            {{ $item->name }}
+                        </a>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
-</div>
+</aside>
