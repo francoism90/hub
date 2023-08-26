638340b84cb7c44a084809dd249bd3355ce620a7
francoism90 <francoism90@users.noreply.github.com>
2023-08-26 12:00:43 +0200

    wip

--- src/App/Web/Videos/Views/index.blade.php
+++ src/App/Web/Videos/Views/index.blade.php
@@ -18,7 +18,7 @@
                         <button
                             @if ($items->onFirstPage()) disabled @endif
                             x-data
-                            @click.window="window.scrollTo({ top: 0, behavior: 'smooth' })"
+                            @click="window.scrollTo({ top: 0, behavior: 'smooth' })"
                             class="cursor-auto text-gray-300 disabled:opacity-50"
                             wire:click="previousPage"
                             wire:loading.attr="disabled"
@@ -33,7 +33,7 @@
                         <button
                             @if ($items->onLastPage()) disabled @endif
                             x-data
-                            @click.window="window.scrollTo({ top: 0, behavior: 'smooth' })"
+                            @click="window.scrollTo({ top: 0, behavior: 'smooth' })"
                             class="cursor-auto text-gray-300 disabled:opacity-50"
                             wire:click="nextPage"
                             wire:loading.attr="disabled"
