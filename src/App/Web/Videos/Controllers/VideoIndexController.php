77bcb39896d276989e7bd0015ed85f45aefb2e75
francoism90 <francoism90@users.noreply.github.com>
2023-08-27 10:09:20 +0200

    wip

--- src/App/Web/Videos/Controllers/VideoIndexController.php
+++ src/App/Web/Videos/Controllers/VideoIndexController.php
@@ -2,7 +2,10 @@
 
 namespace App\Web\Videos\Controllers;
 
-use App\Web\Videos\Concerns\WithFilters;
+use App\Web\Tags\Concerns\WithTags;
+use App\Web\Videos\Concerns\WithVideos;
+use Domain\Tags\Collections\TagCollection;
+use Domain\Tags\Models\Tag;
 use Domain\Videos\Models\Video;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Builder;
@@ -10,10 +13,12 @@ use Illuminate\View\View;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Livewire\Attributes\Computed;
 
 class VideoIndexController extends Component
 {
-    use WithFilters;
+    use WithVideos;
+    use WithTags;
     use WithPagination;
 
     #[Url(history: true)]
@@ -22,9 +27,21 @@ class VideoIndexController extends Component
     #[Url(history: true)]
     public ?string $tag = null;
 
-    public function boot(): void
+    public ?string $type = 'genre';
+
+    public function mount(): void
     {
-        $this->authorize('viewAny', Video::class);
+        if (blank($this->tag)) {
+            return;
+        }
+
+        $types = $this->tagTypes();
+
+        $tag = $this->findTagModel($this->tag);
+
+        $this->type = $tag instanceof Tag && filled($tag->type)
+            ? $types->first(fn (string $type) => $tag->type->value === $type)
+            : $types->first();
     }
 
     public function render(): View
@@ -32,6 +49,38 @@ class VideoIndexController extends Component
         return view('videos::index', [
             'items' => $this->builder(),
         ]);
+    }
+
+    public function setTag(Tag $model): void
+    {
+        $this->tag = ($model->getRouteKey() === $this->tag)
+            ? $this->reset('tag')
+            : $model->getRouteKey();
+
+        $this->resetPage();
+    }
+
+    public function toggleType(): void
+    {
+        $types = $this->tagTypes();
+
+        $this->type = $types->after($this->type, $types->first());
+    }
+
+    #[Computed]
+    public function tags(): TagCollection
+    {
+        return Tag::query()
+            ->type($this->type)
+            ->orderBy('name')
+            ->get()
+            ->sortByDesc(fn (Tag $item) => $item->getRouteKey() === $this->tag);
+    }
+
+    #[Computed]
+    public function tagLabel(): ?string
+    {
+        return $this->findTagType($this->type)?->label;
     }
 
     protected function builder(): Paginator
