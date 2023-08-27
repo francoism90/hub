10560ec8e142c2b53b4894e1a5f74f2a4eb99771
francoism90 <francoism90@users.noreply.github.com>
2023-08-27 19:12:45 +0200

    wip

--- src/Foundation/Providers/LivewireServiceProvider.php
+++ src/Foundation/Providers/LivewireServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace Foundation\Providers;
 
-use App\Web\Layouts\Components\Navbar as LayoutNavbar;
 use App\Web\Layouts\Components\Search as LayoutSearch;
-use App\Web\Videos\Components\Filters as VideosFilter;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 use Support\Livewire\ModelSynth;
@@ -38,8 +36,6 @@ class LivewireServiceProvider extends ServiceProvider
 
     protected function registerComponents(): void
     {
-        Livewire::component('layout-navbar', LayoutNavbar::class);
         Livewire::component('layout-search', LayoutSearch::class);
-        Livewire::component('videos-filter', VideosFilter::class);
     }
 }
