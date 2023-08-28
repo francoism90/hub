1b5d54a476a8f35da27b12d4fc247d9d19805e86
francoism90 <francoism90@users.noreply.github.com>
2023-08-28 18:43:23 +0200

    wip

--- src/App/Admin/Resources/PlaylistResource/Pages/EditPlaylist.php
+++ src/App/Admin/Resources/PlaylistResource/Pages/EditPlaylist.php
@@ -4,14 +4,25 @@ namespace App\Admin\Resources\PlaylistResource\Pages;
 
 use App\Admin\Concerns\InteractsWithFormData;
 use App\Admin\Resources\PlaylistResource;
+use App\Admin\Resources\PlaylistResource\Forms\GeneralForm;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Contracts\Support\Htmlable;
+use Filament\Forms\Form;
 
 class EditPlaylist extends EditRecord
 {
     use InteractsWithFormData;
 
     protected static string $resource = PlaylistResource::class;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->columns(1)
+            ->schema([
+                ...GeneralForm::make(),
+            ]);
+    }
 
     public function getTitle(): string|Htmlable
     {
