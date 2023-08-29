6bd868858b97c2c012497e843d81da3a33a6d69b
francoism90 <francoism90@users.noreply.github.com>
2023-08-29 14:07:19 +0200

    wip

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,3 +1,6 @@
 @if (filled($brand = filament()->getBrandName()))
-    <x-heroicon-o-cloud-arrow-up class="h-[20rem] w-[20rem] !fill-white !text-white" />
+    <div class="flex items-center space-x-4 text-2xl font-medium lowercase">
+        <x-heroicon-s-play-circle class="h-12 w-12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-0.5" />
+        <span>{{ config('app.name') }}</span>
+    </div>
 @endif
