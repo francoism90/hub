a61d0b218778b92452721aaf2c80363af72b4511
francoism90 <francoism90@users.noreply.github.com>
2023-08-29 14:08:41 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,5 +26,8 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
