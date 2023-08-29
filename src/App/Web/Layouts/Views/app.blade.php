73b36c7a72b63024abbdc7ccba03e95c186a308e
francoism90 <francoism90@users.noreply.github.com>
2023-08-29 14:22:26 +0200

    wip

--- src/App/Web/Layouts/Views/app.blade.php
+++ src/App/Web/Layouts/Views/app.blade.php
@@ -9,9 +9,9 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#39336c" />
-    <meta name="msapplication-TileColor" content="#39336c" />
-    <meta name="theme-color" content="#39336c" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ec4899" />
+    <meta name="msapplication-TileColor" content="#ec4899" />
+    <meta name="theme-color" content="#ec4899" />
     {!! SEOMeta::generate() !!}
     <style>
         [x-cloak] {
