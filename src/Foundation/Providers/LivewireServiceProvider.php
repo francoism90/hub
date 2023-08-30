d2da1ed9c47097699c74da91726fab396e465c5f
francoism90 <francoism90@users.noreply.github.com>
2023-08-30 20:12:45 +0200

    wip

--- src/Foundation/Providers/LivewireServiceProvider.php
+++ src/Foundation/Providers/LivewireServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Foundation\Providers;
 
 use App\Web\Filters\Components\Tags as FilterTags;
+use App\Web\Layouts\Components\Search as LayoutSearch;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 use Support\Livewire\ModelSynth;
@@ -36,6 +37,7 @@ class LivewireServiceProvider extends ServiceProvider
 
     protected function registerComponents(): void
     {
+        Livewire::component('layout-search', LayoutSearch::class);
         Livewire::component('filter-tags', FilterTags::class);
     }
 }
