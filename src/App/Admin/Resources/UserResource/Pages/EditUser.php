86c8fa0ef4513d95b7684b5c1d70038e3df84e73
francoism90 <francoism90@users.noreply.github.com>
2023-08-30 14:12:01 +0200

    wip

--- src/App/Admin/Resources/UserResource/Pages/EditUser.php
+++ src/App/Admin/Resources/UserResource/Pages/EditUser.php
@@ -2,14 +2,18 @@
 
 namespace App\Admin\Resources\UserResource\Pages;
 
+use App\Admin\Concerns\InteractsWithAuthentication;
 use App\Admin\Concerns\InteractsWithFormData;
 use App\Admin\Resources\UserResource;
+use Domain\Users\Actions\RegenerateUser;
+use Domain\Users\Models\User;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Contracts\Support\Htmlable;
 
 class EditUser extends EditRecord
 {
+    use InteractsWithAuthentication;
     use InteractsWithFormData;
 
     protected static string $resource = UserResource::class;
@@ -27,8 +31,10 @@ class EditUser extends EditRecord
     {
         return [
             // Custom actions
-            Actions\Action::make()
-                ->label('Regenerate'),
+            Actions\Action::make('regenerate')
+                ->label('Regenerate')
+                ->visible(static::hasRole('super-admin'))
+                ->action(fn (User $record) => app(RegenerateUser::class)->execute($record)),
 
             // Filament actions
             Actions\DeleteAction::make(),
