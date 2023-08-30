27245365e2fbc1fe82712be4bc8d1208e2b806c9
francoism90 <francoism90@users.noreply.github.com>
2023-08-30 14:56:32 +0200

    wip

--- src/App/Web/Profile/Controllers/HistoryController.php
+++ src/App/Web/Profile/Controllers/HistoryController.php
@@ -2,18 +2,24 @@
 
 namespace App\Web\Profile\Controllers;
 
+use App\Web\Playlists\Concerns\WithHistory;
+use App\Web\Profile\Concerns\WithAuthentication;
 use App\Web\Videos\Components\Listing;
 use Artesaos\SEOTools\Facades\SEOMeta;
 use Domain\Playlists\Models\Playlist;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\View\View;
+use Livewire\Component;
+use Livewire\WithPagination;
 
-class HistoryController extends Listing
+class HistoryController extends Component
 {
+    use WithAuthentication;
+    use WithHistory;
+    use WithPagination;
+
     public function mount(): void
     {
-        parent::mount();
-
         SEOMeta::setTitle(__('History'));
     }
 
@@ -26,9 +32,7 @@ class HistoryController extends Listing
 
     protected function builder(): Paginator
     {
-        return Playlist::query()
-            ->history()
-            ->first()
+        return $this->getHistory()
             ->videos()
             ->with('tags')
             ->orderByDesc('videoables.updated_at')
