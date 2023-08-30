368e3e115c8c5221b70d7ffafdb5ec81620f75bb
francoism90 <francoism90@users.noreply.github.com>
2023-08-30 18:05:38 +0200

    wip

--- src/Foundation/Providers/ViewServiceProvider.php
+++ src/Foundation/Providers/ViewServiceProvider.php
@@ -31,6 +31,7 @@ class ViewServiceProvider extends ServiceProvider
         $items = collect([
             ['namespace' => 'App\\Web\\Layouts\\Components', 'prefix' => 'layouts'],
             ['namespace' => 'App\\Web\\Profile\\Components', 'prefix' => 'profile'],
+            ['namespace' => 'App\\Web\\Filters\\Components', 'prefix' => 'filters'],
             ['namespace' => 'App\\Web\\Tags\\Components', 'prefix' => 'tags'],
             ['namespace' => 'App\\Web\\Videos\\Components', 'prefix' => 'videos'],
             ['namespace' => 'App\\Web\\Playlists\\Components', 'prefix' => 'playlists'],
@@ -44,6 +45,7 @@ class ViewServiceProvider extends ServiceProvider
         $items = collect([
             ['path' => app_path('Web/Layouts/Views'), 'namespace' => 'layouts'],
             ['path' => app_path('Web/Profile/Views'), 'namespace' => 'profile'],
+            ['path' => app_path('Web/Filters/Views'), 'namespace' => 'filters'],
             ['path' => app_path('Web/Tags/Views'), 'namespace' => 'tags'],
             ['path' => app_path('Web/Videos/Views'), 'namespace' => 'videos'],
             ['path' => app_path('Web/Playlists/Views'), 'namespace' => 'playlists'],
