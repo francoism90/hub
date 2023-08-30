ff755707e5c85801db5c04acbf897936fe1d14cc
francoism90 <francoism90@users.noreply.github.com>
2023-08-30 14:58:38 +0200

    wip

--- src/App/Web/Profile/Controllers/HistoryController.php
+++ src/App/Web/Profile/Controllers/HistoryController.php
@@ -4,17 +4,16 @@ namespace App\Web\Profile\Controllers;
 
 use App\Web\Playlists\Concerns\WithHistory;
 use App\Web\Profile\Concerns\WithAuthentication;
+use App\Web\Videos\Components\Listing;
 use Artesaos\SEOTools\Facades\SEOMeta;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\View\View;
-use Livewire\Component;
 use Livewire\WithPagination;
 
-class HistoryController extends Component
+class HistoryController extends Listing
 {
     use WithAuthentication;
     use WithHistory;
-    use WithPagination;
 
     public function mount(): void
     {
--- src/App/Web/Videos/Components/Listing.php
+++ src/App/Web/Videos/Components/Listing.php
@@ -4,11 +4,8 @@ namespace App\Web\Videos\Components;
 
 use App\Web\Tags\Concerns\WithTags;
 use App\Web\Videos\Concerns\WithVideos;
-use Domain\Tags\Collections\TagCollection;
-use Domain\Tags\Models\Tag;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\View\View;
-use Livewire\Attributes\Computed;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -25,41 +22,9 @@ abstract class Listing extends Component
     #[Url(history: true)]
     public ?string $tag = '';
 
-    #[Url(history: true)]
-    public ?string $type = '';
-
     abstract public function render(): View;
 
     abstract protected function builder(): Paginator;
-
-    public function mount(): void
-    {
-        $types = $this->tagTypes();
-
-        $this->type = (filled($this->tag) && $tag = $this->findTagModel($this->tag))
-            ? $types->first(fn (string $type) => $tag->type?->value === $type)
-            : $types->first();
-    }
-
-    public function setTag(Tag $model): void
-    {
-        if ($model->getRouteKey() === $this->tag) {
-            $this->resetQuery('tag');
-
-            return;
-        }
-
-        $this->tag = $model->getRouteKey();
-
-        $this->resetPage();
-    }
-
-    public function toggleType(): void
-    {
-        $types = $this->tagTypes();
-
-        $this->type = $types->after($this->type, $types->first());
-    }
 
     public function resetQuery(...$properties): void
     {
@@ -68,27 +33,5 @@ abstract class Listing extends Component
             ->map(fn (string $property) => $this->reset($property));
 
         $this->resetPage();
-    }
-
-    #[Computed]
-    public function tags(): TagCollection
-    {
-        return Tag::query()
-            ->type($this->type)
-            ->orderBy('name')
-            ->get()
-            ->sortByDesc(fn (Tag $item) => $item->getRouteKey() === $this->tag);
-    }
-
-    #[Computed]
-    public function tagType(): ?string
-    {
-        return $this->findTagType($this->type)?->label;
-    }
-
-    #[Computed]
-    public function tagName(): mixed
-    {
-        return $this->findTagModel($this->tag)?->name;
     }
 }
