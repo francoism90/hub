3fe19571aefa6302063b39a2842de0435bd44808
francoism90 <francoism90@users.noreply.github.com>
2023-08-30 16:14:55 +0200

    wip

--- src/Foundation/Providers/ViewServiceProvider.php
+++ src/Foundation/Providers/ViewServiceProvider.php
@@ -4,6 +4,7 @@ namespace Foundation\Providers;
 
 use Artesaos\SEOTools\Facades\SEOMeta;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider
@@ -28,29 +29,50 @@ class ViewServiceProvider extends ServiceProvider
 
     protected function configureComponents(): void
     {
-        $items = collect([
-            ['namespace' => 'App\\Web\\Layouts\\Components', 'prefix' => 'layouts'],
-            ['namespace' => 'App\\Web\\Filters\\Components', 'prefix' => 'filters'],
-            ['namespace' => 'App\\Web\\Profile\\Components', 'prefix' => 'profile'],
-            ['namespace' => 'App\\Web\\Tags\\Components', 'prefix' => 'tags'],
-            ['namespace' => 'App\\Web\\Videos\\Components', 'prefix' => 'videos'],
-            ['namespace' => 'App\\Web\\Playlists\\Components', 'prefix' => 'playlists'],
-        ]);
+        $domains = File::directories(app_path());
 
-        $items->each(fn (array $item) => Blade::componentNamespace(...$item));
+        collect($domains)
+            ->flatMap(fn (string $domain) => File::directories($domain))
+            ->filter(fn (string $path) => File::isDirectory("{$path}/Components"))
+            ->each(function (string $path) {
+                // e.g. web
+                $domain = basename(str($path)->beforeLast('/')->lower());
+
+                // e.g. filters
+                $key = basename(str($path)->afterLast('/')->lower());
+
+                $namespace = str("{$path}/Components")
+                    ->replaceFirst(app_path(), 'App\\')
+                    ->replace('/', '\\')
+                    ->trim('\\');
+
+                Blade::componentNamespace(
+                    namespace: $namespace,
+                    prefix: implode('.', [$domain, $key])
+                );
+            });
     }
 
     protected function configureViews(): void
     {
-        $items = collect([
-            ['path' => app_path('Web/Layouts/Views'), 'namespace' => 'layouts'],
-            ['path' => app_path('Web/Filters/Views'), 'namespace' => 'filters'],
-            ['path' => app_path('Web/Profile/Views'), 'namespace' => 'profile'],
-            ['path' => app_path('Web/Tags/Views'), 'namespace' => 'tags'],
-            ['path' => app_path('Web/Videos/Views'), 'namespace' => 'videos'],
-            ['path' => app_path('Web/Playlists/Views'), 'namespace' => 'playlists'],
-        ]);
+        $domains = File::directories(app_path());
 
-        $items->each(fn (array $item) => $this->loadViewsFrom(...$item));
+        collect($domains)
+            ->flatMap(fn (string $domain) => File::directories($domain))
+            ->filter(fn (string $path) => File::isDirectory("{$path}/Views"))
+            ->each(function (string $path) {
+                // e.g. web
+                $domain = basename(str($path)->beforeLast('/')->lower());
+
+                // e.g. filters
+                $key = basename(str($path)->afterLast('/')->lower());
+
+                // dd($path);
+
+                Blade::componentNamespace(
+                    namespace: $path,
+                    prefix: implode('.', [$domain, $key])
+                );
+            });
     }
 }
