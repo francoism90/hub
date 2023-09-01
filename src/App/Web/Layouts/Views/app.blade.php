302c4d64e61bea1a82617298dc59204705819bd2
francoism90 <francoism90@users.noreply.github.com>
2023-09-01 20:00:15 +0200

    Use build

--- src/App/Web/Layouts/Views/app.blade.php
+++ src/App/Web/Layouts/Views/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="msapplication-TileColor" content="#ec4899">
     <meta name="theme-color" content="#ec4899">
-    <link rel="manifest" href="/manifest.webmanifest">
+    <link rel="manifest" href="/build/manifest.webmanifest">
     <link rel="apple-touch-icon" sizes="180x180" href="/storage/images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/storage/images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/storage/images/favicon-16x16.png">
