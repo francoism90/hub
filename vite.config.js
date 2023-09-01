d9cf698e62111081ac1efa8f3c73ad7b07e8cbe0
francoism90 <francoism90@users.noreply.github.com>
2023-09-01 18:30:40 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -58,17 +58,17 @@ export default defineConfig({
           {
             src: '/storage/images/android-chrome-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: '/storage/images/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
+            purpose: 'any maskable',
+          },
+        ],
       },
-    })
+    }),
   ],
   build: {
     chunkSizeWarningLimit: 1024,
