9f6e7f9c3e947570a5a5ddb4a4d3e9c93e155e25
francoism90 <francoism90@users.noreply.github.com>
2023-09-02 10:32:33 +0200

    wip

--- src/App/Web/Videos/Views/item.blade.php
+++ src/App/Web/Videos/Views/item.blade.php
@@ -1,37 +1,35 @@
 <article class="rounded bg-gray-700/40 p-4 shadow-md">
-    <div class="flex flex-row flex-nowrap items-center space-x-4">
-        <div class="h-16 w-16 flex-none">
-            <a href="{{ route('videos.view', $item) }}">
+    <a href="{{ route('videos.view', $item) }}">
+        <div class="flex flex-row flex-nowrap items-center space-x-4">
+            <div class="h-16 w-16 flex-none">
                 <img
                     alt="{{ $item->name }}"
                     src="{{ $item->thumbnail }}"
                     class="h-full w-full bg-black object-cover text-transparent"
                     crossorigin="use-credentials"
                     loading="lazy" />
-            </a>
-        </div>
+            </div>
 
-        <div class="grow">
-            <div class="flex flex-col space-y-1.5">
-                <dl>
-                    <dt class="sr-only">Published on</dt>
-                    <dd class="hidden text-ellipsis text-sm font-medium leading-4 text-gray-400 sm:block">
-                        <time datetime="{{ $item->created_at->format('Y-m-d\TH:i:s.uP') }}">
-                            {{ $item->created_at->format('F d, Y') }}
-                        </time>
-                    </dd>
-                </dl>
+            <div class="grow">
+                <div class="flex flex-col space-y-1.5">
+                    <dl>
+                        <dt class="sr-only">Published on</dt>
+                        <dd class="hidden text-ellipsis text-sm font-medium leading-4 text-gray-400 sm:block">
+                            <time datetime="{{ $item->created_at->format('Y-m-d\TH:i:s.uP') }}">
+                                {{ $item->created_at->format('F d, Y') }}
+                            </time>
+                        </dd>
+                    </dl>
 
-                <h2 class="line-clamp-2 text-sm font-bold capitalize leading-6 tracking-tight">
-                    <a href="{{ route('videos.view', $item) }}">
+                    <h2 class="line-clamp-2 text-sm font-bold capitalize leading-6 tracking-tight">
                         {{ $item->name }}
-                    </a>
-                </h2>
+                    </h2>
 
-                @if ($item->tags)
-                    <x-videos::tags :items="$item->tags" />
-                @endif
+                    @if ($item->tags)
+                        <x-videos::tags :items="$item->tags" />
+                    @endif
+                </div>
             </div>
         </div>
-    </div>
+    </a>
 </article>
