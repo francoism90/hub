dba2424887db0cfe47a254ca90c0b32aa7ba268d
francoism90 <francoism90@users.noreply.github.com>
2023-09-08 12:42:25 +0200

    Fix tag filter

--- src/Domain/Videos/Support/QueryBuilder/Filters/TagFilter.php
+++ src/Domain/Videos/Support/QueryBuilder/Filters/TagFilter.php
@@ -10,7 +10,7 @@ class TagFilter implements Filter
     public function __invoke(Builder $query, mixed $value, string $property): void
     {
         $query->when(filled($value), fn (Builder $query) => $query
-            ->tags($value)
+            ->tagged($value)
         );
     }
 }
