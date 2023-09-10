101f1ded6d94d359a5f07e8674e3e4810cdd2512
francoism90 <francoism90@users.noreply.github.com>
2023-09-10 16:53:43 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,18 @@ use Illuminate\Support\Facades\Route;
 
 // Profile
 Route::name('profile.')->middleware('auth')->group(function () {
-    Route::get('/history', HistoryController::class)->name('history')->lazy();
-    Route::get('/favorites', FavoritesController::class)->name('favorites')->lazy();
-    Route::get('/watchlist', WatchlistController::class)->name('watchlist')->lazy();
+    Route::get('/history', HistoryController::class)->name('history');
+    Route::get('/favorites', FavoritesController::class)->name('favorites');
+    Route::get('/watchlist', WatchlistController::class)->name('watchlist');
 });
 
 // Videos
 Route::name('videos.')->middleware('auth')->group(function () {
-    Route::get('/', VideoIndexController::class)->name('index')->lazy();
-    Route::get('/video/{video}', VideoViewController::class)->name('view')->lazy(enabled: false);
+    Route::get('/', VideoIndexController::class)->name('index');
+    Route::get('/video/{video}', VideoViewController::class)->name('view');
 });
 
 // Tags
 Route::name('tags.')->prefix('tags')->middleware('auth')->group(function () {
-    Route::get('/', TagIndexController::class)->name('index')->lazy();
+    Route::get('/', TagIndexController::class)->name('index');
 });
