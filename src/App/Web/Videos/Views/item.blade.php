62f2ecaab64c84fcce5d8a49ac158a642ea75ac5
francoism90 <francoism90@users.noreply.github.com>
2023-09-12 16:07:01 +0200

    Add intersect

--- src/App/Web/Videos/Views/item.blade.php
+++ src/App/Web/Videos/Views/item.blade.php
@@ -1,42 +1,47 @@
-<article class="flex flex-row flex-nowrap items-center space-x-4">
-    <a
-        class="flex-none"
-        href="{{ route('videos.view', $item) }}">
-        <img
-            alt="{{ $item->title }}"
-            src="{{ $item->thumbnail }}"
-            class="h-20 w-36 bg-black object-cover text-transparent"
-            crossorigin="use-credentials"
-            loading="lazy" />
-    </a>
+<article
+    x-data="{ shown: false }"
+    x-intersect.once="shown = true"
+    class="h-20 max-h-[5rem] min-h-[5rem] w-full">
+    <div class="flex flex-row flex-nowrap items-center space-x-4" x-show="shown" x-transition>
+        <a
+            class="flex-none"
+            href="{{ route('videos.view', $item) }}">
+            <img
+                alt="{{ $item->title }}"
+                src="{{ $item->thumbnail }}"
+                class="h-20 w-36 bg-black object-cover text-transparent"
+                crossorigin="use-credentials"
+                loading="lazy" />
+        </a>
 
-    <div class="grow">
-        <div class="flex flex-col">
-            <a href="{{ route('videos.view', $item) }}">
-                <h2 class="line-clamp-1 text-sm font-semibold capitalize tracking-tight">
-                    {{ $item->title }}
-                </h2>
+        <div class="grow">
+            <div class="flex flex-col">
+                <a href="{{ route('videos.view', $item) }}">
+                    <h2 class="line-clamp-1 text-sm font-semibold capitalize tracking-tight">
+                        {{ $item->title }}
+                    </h2>
 
-                <dl>
-                    <dt class="sr-only">Duration</dt>
-                    <dd class="text-ellipsis text-xs font-medium text-gray-400">
-                        <time>
-                            {{ duration($item->duration) }}
-                        </time>
-                    </dd>
-
-                    @if ($item->episode || $item->season)
-                        <dt class="sr-only">Identifier</dt>
-                        <dd class="text-xs font-medium text-gray-400">
-                            {{ implode('', [$item->season, $item->episode]) }}
+                    <dl>
+                        <dt class="sr-only">Duration</dt>
+                        <dd class="text-ellipsis text-xs font-medium text-gray-400">
+                            <time>
+                                {{ duration($item->duration) }}
+                            </time>
                         </dd>
-                    @endif
-                </dl>
-            </a>
 
-            @if ($item->tags->isNotEmpty())
-                <x-videos::tags class="line-clamp-1 text-xs" :items="$item->tags" />
-            @endif
+                        @if ($item->episode || $item->season)
+                            <dt class="sr-only">Identifier</dt>
+                            <dd class="text-xs font-medium text-gray-400">
+                                {{ implode('', [$item->season, $item->episode]) }}
+                            </dd>
+                        @endif
+                    </dl>
+                </a>
+
+                @if ($item->tags->isNotEmpty())
+                    <x-videos::tags class="line-clamp-1 text-xs" :items="$item->tags" />
+                @endif
+            </div>
         </div>
     </div>
 </article>
