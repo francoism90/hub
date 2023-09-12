9d4d37fcf9d158e53cb0de4656fe625b4732d4cb
francoism90 <francoism90@users.noreply.github.com>
2023-09-12 19:30:10 +0200

    wip

--- src/App/Web/Search/Views/filters.blade.php
+++ src/App/Web/Search/Views/filters.blade.php
@@ -1,67 +1,69 @@
 <div class="flex w-full flex-row flex-nowrap space-x-6">
-    <x-layouts::dropdown>
-        <button class="btn text-sm font-semibold">
-            <span>{{ $this->sorter }}</span>
-            <x-heroicon-m-chevron-down
-                class="h-4 w-4"
-                x-bind:class="open ? 'rotate-180' : ''" />
-        </button>
+    @if (filled($this->form->query))
+        <x-layouts::dropdown>
+            <button class="btn text-sm font-semibold">
+                <span>{{ $this->sorter }}</span>
+                <x-heroicon-m-chevron-down
+                    class="h-4 w-4"
+                    x-bind:class="open ? 'rotate-180' : ''" />
+            </button>
 
-        <x-slot:content>
-            <div
-                @click.away="open = false"
-                class="absolute left-0 top-8 w-44 min-w-[11rem] bg-gray-900 py-2">
-                @foreach ($this->sorters as $key => $label)
-                    <label
-                        for="sort-{{ $key }}"
-                        class="btn @if ($this->hasSort($key)) btn-gradient @endif justify-start px-4 py-2 text-sm">
-                        <span>{{ $label }}</span>
-                        @if ($this->hasSort($key))
-                            <x-heroicon-o-check class="h-4 w-4" />
-                        @endif
-                    </label>
+            <x-slot:content>
+                <div
+                    @click.away="open = false"
+                    class="absolute left-0 top-8 w-44 min-w-[11rem] bg-gray-900 py-2">
+                    @foreach ($this->sorters as $key => $label)
+                        <label
+                            for="sort-{{ $key }}"
+                            class="btn @if ($this->hasSort($key)) btn-gradient @endif justify-start px-4 py-2 text-sm">
+                            <span>{{ $label }}</span>
+                            @if ($this->hasSort($key))
+                                <x-heroicon-o-check class="h-4 w-4" />
+                            @endif
+                        </label>
 
-                    <input
-                        type="radio"
-                        class="hidden"
-                        id="sort-{{ $key }}"
-                        value="{{ $key }}"
-                        wire:model.live="form.sort" />
-                @endforeach
-            </div>
-        </x-slot:content>
-    </x-layouts::dropdown>
+                        <input
+                            type="radio"
+                            class="hidden"
+                            id="sort-{{ $key }}"
+                            value="{{ $key }}"
+                            wire:model.live="form.sort" />
+                    @endforeach
+                </div>
+            </x-slot:content>
+        </x-layouts::dropdown>
 
-    <x-layouts::dropdown>
-        <button class="btn text-sm font-semibold">
-            <span>{{ __('Features') }}</span>
-            <x-heroicon-m-chevron-down
-                class="h-4 w-4"
-                x-bind:class="open ? 'rotate-180' : ''" />
-        </button>
+        <x-layouts::dropdown>
+            <button class="btn text-sm font-semibold">
+                <span>{{ __('Features') }}</span>
+                <x-heroicon-m-chevron-down
+                    class="h-4 w-4"
+                    x-bind:class="open ? 'rotate-180' : ''" />
+            </button>
 
-        <x-slot:content>
-            <div
-                @click.away="open = false"
-                class="absolute left-0 top-8 w-44 min-w-[11rem] bg-gray-900 py-2">
-                @foreach ($this->features as $key => $label)
-                    <label
-                        for="feature-{{ $key }}"
-                        class="btn @if ($this->hasFeature($key)) btn-gradient @endif justify-start px-4 py-2 text-sm">
-                        <span>{{ $label }}</span>
-                        @if ($this->hasFeature($key))
-                            <x-heroicon-o-check class="h-4 w-4" />
-                        @endif
-                    </label>
+            <x-slot:content>
+                <div
+                    @click.away="open = false"
+                    class="absolute left-0 top-8 w-44 min-w-[11rem] bg-gray-900 py-2">
+                    @foreach ($this->features as $key => $label)
+                        <label
+                            for="feature-{{ $key }}"
+                            class="btn @if ($this->hasFeature($key)) btn-gradient @endif justify-start px-4 py-2 text-sm">
+                            <span>{{ $label }}</span>
+                            @if ($this->hasFeature($key))
+                                <x-heroicon-o-check class="h-4 w-4" />
+                            @endif
+                        </label>
 
-                    <input
-                        type="checkbox"
-                        class="hidden"
-                        id="feature-{{ $key }}"
-                        value="{{ $key }}"
-                        wire:model.live="form.feature" />
-                @endforeach
-            </div>
-        </x-slot:content>
-    </x-layouts::dropdown>
+                        <input
+                            type="checkbox"
+                            class="hidden"
+                            id="feature-{{ $key }}"
+                            value="{{ $key }}"
+                            wire:model.live="form.feature" />
+                    @endforeach
+                </div>
+            </x-slot:content>
+        </x-layouts::dropdown>
+    @endif
 </div>
--- src/App/Web/Search/Views/items.blade.php
+++ src/App/Web/Search/Views/items.blade.php
@@ -1,15 +1,13 @@
 <div class="flex flex-col space-y-8 py-8">
-    @if (filled($this->form->query) && blank($this->items))
-        <x-search::filters />
+    <x-search::filters />
 
+    @if (filled($this->form->query) && blank($this->items))
         <div class="flex w-full items-center justify-center p-8 text-gray-400">
             {{ __('No results match your search criteria') }}
         </div>
     @endif
 
     @if (filled($this->form->query) && filled($this->items))
-        <x-search::filters />
-
         <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
             @foreach ($this->items as $item)
                 <x-videos::item :$item />
