8894d60230390686fa968a1abd54d7ccbd457f53
francoism90 <francoism90@users.noreply.github.com>
2023-09-14 08:55:04 +0200

    Fix query padding

--- src/App/Web/Search/Views/query.blade.php
+++ src/App/Web/Search/Views/query.blade.php
@@ -1,15 +1,17 @@
-<div class="m-4 flex w-full flex-row flex-nowrap items-center space-x-4 rounded bg-gray-800 px-4">
-    <input
-        class="input grow bg-transparent px-0 py-3 text-sm text-gray-300 placeholder:text-gray-500"
-        type="search"
-        placeholder="{{ __('Search on title, actor or studio') }}"
-        autocomplete
-        autofocus
-        wire:model.live.debounce.400ms="form.query" />
+<div class="p-4">
+    <div class="flex w-full flex-row flex-nowrap items-center space-x-4 rounded bg-gray-800 px-4">
+        <input
+            class="input grow bg-transparent px-0 py-3 text-sm text-gray-300 placeholder:text-gray-500"
+            type="search"
+            placeholder="{{ __('Search on title, actor or studio') }}"
+            autocomplete
+            autofocus
+            wire:model.live.debounce.400ms="form.query" />
 
-    @if (filled($this->form->query))
-        <a class="cursor-pointer" wire:click="$set('form.query', '')">
-            <x-heroicon-o-x-mark class="h-5 w-5" />
-        </a>
-    @endif
+        @if (filled($this->form->query))
+            <a class="cursor-pointer" wire:click="$set('form.query', '')">
+                <x-heroicon-o-x-mark class="h-5 w-5" />
+            </a>
+        @endif
+    </div>
 </div>
