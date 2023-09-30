3d238a608740959dc65a7d6b776a747754877cd8
francoism90 <francoism90@users.noreply.github.com>
2023-09-30 16:15:17 +0200

    Add tracking-tight

--- src/App/Web/Videos/Views/tags.blade.php
+++ src/App/Web/Videos/Views/tags.blade.php
@@ -1,7 +1,7 @@
-<div {{ $attributes->class('flex flex-wrap gap-x-2.5 gap-y-1 text-sm font-medium') }}>
+<div {{ $attributes->class('flex flex-wrap gap-x-2.5 gap-y-1 text-sm') }}>
     @foreach ($items as $item)
         <a
-            class="uppercase text-primary-500 hover:text-primary-400"
+            class="font-medium uppercase tracking-tight text-primary-500 hover:text-primary-400"
             href="{{ route('videos.index', ['t[]' => $item->getRouteKey()]) }}"
             wire:navigate>
             {{ $item->name }}
