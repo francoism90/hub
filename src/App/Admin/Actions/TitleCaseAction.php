4ed02c7968e2a588cf19c59ae834d3b718d8d5c6
francoism90 <francoism90@users.noreply.github.com>
2023-10-26 18:23:41 +0200

    Check is string

--- src/App/Admin/Actions/TitleCaseAction.php
+++ src/App/Admin/Actions/TitleCaseAction.php
@@ -25,7 +25,7 @@ class TitleCaseAction extends Action
 
         $this->hiddenLabel();
 
-        $this->disabled(fn (mixed $state): bool => blank($state));
+        $this->disabled(fn (mixed $state): bool => blank($state) || ! is_string($state));
 
         $this->action(function (): void {
             $this->process(function (Component $component, mixed $state) {
