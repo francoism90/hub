b204ac8af550b5028aaf1f2a50b93db61a8e7b01
francoism90 <francoism90@users.noreply.github.com>
2023-11-13 22:25:02 +0100

    wip

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  define: {
-    'import.meta.vitest': 'undefined',
-  },
   server: {
     hmr: undefined,
     https: false
