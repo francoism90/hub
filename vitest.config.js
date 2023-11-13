14d4aab66522d2c708586fe12f4de44e4d3be413
francoism90 <francoism90@users.noreply.github.com>
2023-11-13 21:52:03 +0100

    wip

--- vitest.config.js
+++ vitest.config.js
@@ -3,10 +3,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    host: 'localhost',
-    port: 5173,
-    strictPort: true,
-    hmr: undefined,
-    https: false,
+    https: false
   },
 })
