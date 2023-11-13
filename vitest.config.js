0b18318153bfe3fc432bf5406ee2f7de599d0e6e
francoism90 <francoism90@users.noreply.github.com>
2023-11-13 22:16:24 +0100

    wip

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
-
+import { fileURLToPath, URL } from 'url'
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
 
 export default defineConfig({
   server: {
+    hmr: undefined,
     https: false
+  },
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('./node_modules', import.meta.url)),
+      '!': fileURLToPath(new URL('./vendor', import.meta.url)),
+    },
+  },
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+        'resources/css/filament/admin/theme.css',
+      ],
+      refresh: [...refreshPaths, 'resources/**', 'src/**'],
+    })
+  ],
+  build: {
+    chunkSizeWarningLimit: 1024,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          player: ['shaka-player', 'shaka-player/dist/shaka-player.ui'],
+          utils: ['axios'],
+          ws: ['laravel-echo', 'pusher-js'],
+        },
+      },
+    },
   },
 })
