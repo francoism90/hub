27d35b26c3db9227887a618935f9ca976a83e66a
francoism90 <francoism90@users.noreply.github.com>
2023-12-03 11:50:03 +0100

    Move poll to parent

--- src/App/Web/Videos/Views/similar.blade.php
+++ src/App/Web/Videos/Views/similar.blade.php
@@ -1,9 +1,9 @@
-<div class="py-3.5">
+<div class="py-3.5" wire:poll.keep-alive.10s>
     <h2 class="text-sm uppercase tracking-wide text-gray-400">
         {{ __('Similar videos') }}
     </h2>
 
-    <div class="grid grow grid-cols-1 gap-3.5 py-3.5 sm:grid-cols-2" wire:poll.keep-alive.10s>
+    <div class="grid grow grid-cols-1 gap-3.5 py-3.5 sm:grid-cols-2">
         @forelse ($this->items as $item)
             <x-videos::item :$item />
         @empty
