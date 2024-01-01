a774814d74d75b7b8a7a3a05469c076c042692cc
francoism90 <francoism90@users.noreply.github.com>
2024-01-01 16:33:12 +0100

    wip

--- src/App/Web/Search/Views/index.blade.php
+++ src/App/Web/Search/Views/index.blade.php
@@ -1,28 +1,23 @@
-<x-layouts::container>
-    <div class="flex flex-col gap-y-4 p-4">
-        <div class="flex w-full flex-row flex-nowrap items-center gap-x-4 rounded bg-gray-800 px-4">
-            <x-forms-text-input
-                class="input grow bg-transparent px-0 py-3 text-sm text-gray-300 placeholder:text-gray-500"
-                type="search"
-                placeholder="{{ __('Search on title, actor or studio') }}"
-                autocomplete
-                autofocus
-                wire:model.live.debounce.300ms="form.search"
-            />
-
-            {{-- @if (filled($this->form->search))
-                <a
-                    class="cursor-pointer"
-                    wire:click="$set('form.search', '')"
-                >
-                    <x-heroicon-o-x-mark class="h-5 w-5" />
-                </a>
-            @endif --}}
-        </div>
-
+<x-layouts::container class="flex flex-col gap-y-4 p-4">
+    <div class="flex m-4 w-full flex-row flex-nowrap items-center gap-x-4 rounded bg-gray-800 px-4">
+        <x-forms-text-input
+            class="input bg-transparent px-0 py-3 text-sm text-gray-300 placeholder:text-gray-500"
+            type="search"
+            placeholder="{{ __('Search on title, actor or studio') }}"
+            autocomplete
+            autofocus
+            wire:model.live.debounce.300ms="form.search"
+        >
+            <x-slot:append>
+                @if ($this->form->hasSearch())
+                    <button wire:click.prevent="$set('form.search', '')">
+                        <x-heroicon-o-x-mark class="h-5 w-5" />
+                    </button>
+                @endif
+            </x-slot:append>
+        </x-forms-text-input>
     </div>
-        {{-- <x-search::filters /> --}}
 
-    {{-- <x-search::query /> --}}
-    {{-- <x-search::items /> --}}
+    <x-search::filters />
+    <x-search::items />
 </x-layouts::container>
