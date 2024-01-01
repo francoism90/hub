be6b0be2de063cc8a7422cde3037700d1e67b224
francoism90 <francoism90@users.noreply.github.com>
2024-01-01 16:34:20 +0100

    wip

--- src/App/Web/Search/Views/index.blade.php
+++ src/App/Web/Search/Views/index.blade.php
@@ -1,7 +1,7 @@
 <x-layouts::container class="flex flex-col gap-y-4 p-4">
     <div class="flex m-4 w-full flex-row flex-nowrap items-center gap-x-4 rounded bg-gray-800 px-4">
         <x-forms-text-input
-            class="input bg-transparent px-0 py-3 text-sm text-gray-300 placeholder:text-gray-500"
+            class="input bg-transparent px-0 text-sm text-gray-300 placeholder:text-gray-500"
             type="search"
             placeholder="{{ __('Search on title, actor or studio') }}"
             autocomplete
