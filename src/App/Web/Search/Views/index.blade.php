81b69f09febddc906c1f494a49ab079475aa8cc4
francoism90 <francoism90@users.noreply.github.com>
2024-01-01 15:28:20 +0100

    wip

--- src/App/Web/Search/Views/index.blade.php
+++ src/App/Web/Search/Views/index.blade.php
@@ -1,4 +1,30 @@
 <x-layouts::container>
-    <x-search::query />
-    <x-search::items />
+    <div class="flex flex-col gap-y-4 p-4">
+        <div class="flex w-full flex-row flex-nowrap items-center gap-x-4 rounded bg-gray-800 px-4">
+            <x-forms-text-input
+                label="{{ __('foo2') }}"
+                icon="{{ __('foo') }}"
+                class="input grow bg-transparent px-0 py-3 text-sm text-gray-300 placeholder:text-gray-500"
+                type="search"
+                placeholder="{{ __('Search on title, actor or studio') }}"
+                autocomplete
+                autofocus
+                wire:model.live.debounce.300ms="form.search"
+            />
+
+            {{-- @if (filled($this->form->search))
+                <a
+                    class="cursor-pointer"
+                    wire:click="$set('form.search', '')"
+                >
+                    <x-heroicon-o-x-mark class="h-5 w-5" />
+                </a>
+            @endif --}}
+        </div>
+
+    </div>
+        {{-- <x-search::filters /> --}}
+
+    {{-- <x-search::query /> --}}
+    {{-- <x-search::items /> --}}
 </x-layouts::container>
