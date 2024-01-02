4d7c1307dcbf3a403a6d1079bee87799226eb3f0
francoism90 <francoism90@users.noreply.github.com>
2024-01-02 19:47:47 +0100

    wip

--- src/App/Web/Videos/Components/Listing.php
+++ src/App/Web/Videos/Components/Listing.php
@@ -6,10 +6,12 @@ use App\Web\Search\Forms\SearchForm;
 use App\Web\Tags\Concerns\WithTags;
 use App\Web\Videos\Concerns\WithFilters;
 use Foxws\LivewireUse\QueryBuilder\Components\QueryBuilder;
+use Livewire\WithPagination;
 
 abstract class Listing extends QueryBuilder
 {
     use WithFilters;
+    use WithPagination;
     use WithTags;
 
     public SearchForm $form;
