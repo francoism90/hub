d02a918fd40cb220167c5650d0b67b0f163b22db
francoism90 <francoism90@users.noreply.github.com>
2024-01-02 20:34:11 +0100

    wip

--- src/App/Web/Search/Forms/SearchForm.php
+++ src/App/Web/Search/Forms/SearchForm.php
@@ -2,11 +2,16 @@
 
 namespace App\Web\Search\Forms;
 
-use Foxws\LivewireUse\QueryBuilder\Forms\FilterForm;
+use Foxws\LivewireUse\QueryBuilder\Concerns\WithSearch;
+use Foxws\LivewireUse\QueryBuilder\Concerns\WithSorts;
+use Foxws\LivewireUse\QueryBuilder\Forms\QueryBuilderForm;
 use Livewire\Attributes\Validate;
 
-class SearchForm extends FilterForm
+class SearchForm extends QueryBuilderForm
 {
+    use WithSearch;
+    use WithSorts;
+
     #[Validate('nullable|array|in:caption')]
     public ?array $feature = null;
 }
