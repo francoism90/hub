b6592c46d543d7b671f73aebd78ce48ef8db87a9
francoism90 <francoism90@users.noreply.github.com>
2024-01-03 15:17:10 +0100

    wip

--- resources/views/tags/item.blade.php
+++ resources/views/tags/item.blade.php
@@ -1,32 +1,8 @@
-<article
+<x-ui-link
     wire:key="{{ $item->getRouteKey() }}"
-    class="rounded bg-gray-700/40 p-4 shadow-md"
+    class="cursor-pointer text-sm font-medium uppercase"
+    href="{{ route('home', ['t' => $item->getRouteKey()]) }}"
 >
-    <a href="{{ route('home', ['tag' => $item->getRouteKey()]) }}">
-        <div class="flex flex-row flex-nowrap items-center gap-x-4">
-            <div class="flex h-16 w-16 items-center justify-center rounded bg-gray-600 px-2 py-1 text-xs text-gray-200">
-                <x-icon
-                    :name="$icon"
-                    class="h-5 w-5"
-                />
-            </div>
-
-            <div class="grow">
-                <div class="flex flex-col gap-y-1.5">
-                    <h2 class="line-clamp-1 text-sm font-bold capitalize leading-6 tracking-tight">
-                        {{ $item->name }}
-                    </h2>
-
-                    @if (filled($type))
-                        <dl>
-                            <dt class="sr-only">{{ __('Type') }}</dt>
-                            <dd class="text-ellipsis text-sm font-medium leading-4 text-gray-400">
-                                {{ $type }}
-                            </dd>
-                        </dl>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </a>
-</article>
+    <span class="text-primary-500 hover:text-primary-400">{{ $item->name }}</span>
+    <span>({{ $item->videos_count }})</span>
+</x-ui-link>
