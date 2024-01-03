8b1ec7fb5c0452d84d602b4d87ca1a2a6abdc777
francoism90 <francoism90@users.noreply.github.com>
2024-01-03 15:20:56 +0100

    wip

--- src/App/Web/Videos/Components/Similar.php
+++ src/App/Web/Videos/Components/Similar.php
@@ -4,6 +4,8 @@ namespace App\Web\Videos\Components;
 
 use App\Web\Videos\Concerns\WithVideo;
 use Domain\Videos\Actions\GetSimilarVideos;
+use Domain\Videos\Models\Video;
+use Foxws\LivewireUse\Views\Concerns\WithQueryBuilder;
 use Illuminate\Support\LazyCollection;
 use Illuminate\View\View;
 use Livewire\Attributes\Computed;
@@ -13,7 +15,10 @@ use Livewire\Component;
 #[Lazy]
 class Similar extends Component
 {
+    use WithQueryBuilder;
     use WithVideo;
+
+    protected static string $model = Video::class;
 
     public function render(): View
     {
