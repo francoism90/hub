06e2186efad4ec13c072115d39da1b840f0c7f7a
francoism90 <francoism90@users.noreply.github.com>
2024-01-13 10:54:26 +0100

    Use lazyById

--- src/App/Videos/Components/Filters.php
+++ src/App/Videos/Components/Filters.php
@@ -19,7 +19,7 @@ class Filters extends Component
     {
         return Tag::query()
             ->ordered()
-            ->cursor()
+            ->lazyById()
             ->when($items, fn (LazyCollection $collection) => $collection
                 ->sortByDesc(fn (Tag $item) => in_array($item->getRouteKey(), $items))
             );
