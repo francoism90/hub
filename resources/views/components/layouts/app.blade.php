4552f734f04f405115c921047717080fb4fe614b
francoism90 <francoism90@users.noreply.github.com>
2024-01-16 08:41:56 +0100

    Move to app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,29 +3,13 @@
     class="scroll-smooth"
     lang="{{ str_replace('_', '-', app()->getLocale()) }}"
 >
-
 <head>
     <meta charset="utf-8" />
-    <meta
-        name="application-name"
-        content="{{ config('app.name') }}"
-    >
-    <meta
-        name="csrf-token"
-        content="{{ csrf_token() }}"
-    >
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-    >
-    <meta
-        name="msapplication-TileColor"
-        content="#030712"
-    >
-    <meta
-        name="theme-color"
-        content="#030712"
-    >
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="msapplication-TileColor" content="#030712">
+    <meta name="theme-color" content="#030712">
     {!! SEOMeta::generate() !!}
     <link
         rel="apple-touch-icon"
@@ -65,11 +49,11 @@
 
 <body class="antialiased">
 
-    <x-header />
+    <x-app.header />
 
     {{ $slot }}
 
-    <x-footer />
+    <x-app.footer />
 
     @vite('resources/js/app.js')
     @stack('scripts')
