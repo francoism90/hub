6626991a7bf2c9b7ed97d6eb4e44285df8d053e0
francoism90 <francoism90@users.noreply.github.com>
2024-02-23 16:08:49 +0100

    Move tagged to trait

--- src/Domain/Tags/Concerns/InteractsWithTags.php
+++ src/Domain/Tags/Concerns/InteractsWithTags.php
@@ -4,10 +4,22 @@ namespace Domain\Tags\Concerns;
 
 use Domain\Tags\Models\Tag;
 use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 
 trait InteractsWithTags
 {
+    public function tagged(Arrayable|array|Tag|null $values = null): self
+    {
+        $items = static::convertToTags($values);
+
+        return $this->when($items->isNotEmpty(), fn (Builder $query) => $query
+            ->withWhereHas('tags', fn ($query) => $query
+                ->whereIn('tags.id', $items->pluck('id'))
+            )
+        );
+    }
+
     protected static function convertToTags(Arrayable|array|Tag|null $values = null): Collection
     {
         return collect($values)
--- src/Domain/Videos/QueryBuilders/VideoQueryBuilder.php
+++ src/Domain/Videos/QueryBuilders/VideoQueryBuilder.php
@@ -5,12 +5,10 @@ namespace Domain\Videos\QueryBuilders;
 use Domain\Shared\Concerns\InteractsWithRandomSeed;
 use Domain\Shared\Concerns\InteractsWithScout;
 use Domain\Tags\Concerns\InteractsWithTags;
-use Domain\Tags\Models\Tag;
 use Domain\Users\Models\User;
 use Domain\Videos\Actions\GetSimilarVideos;
 use Domain\Videos\Models\Video;
 use Domain\Videos\States\Verified;
-use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Database\Eloquent\Builder;
 
 class VideoQueryBuilder extends Builder
@@ -42,16 +40,6 @@ class VideoQueryBuilder extends Builder
         return $this->when($items->isNotEmpty(), fn (Builder $query) => $query
             ->whereIn('id', $items->pluck('id'))
             ->orderByRaw('FIND_IN_SET (id, ?)', [$items->pluck('id')->implode(',')])
-        );
-    }
-
-    public function tagged(Arrayable|array|Tag|null $values = null): self
-    {
-        $items = static::convertToTags($values);
-
-        return $this->when($items->isNotEmpty(), fn (Builder $query) => $query
-            ->withAnyTagsOfAnyType($items)
-            ->randomSeed(key: 'tags', ttl: now()->addDay())
         );
     }
 }
