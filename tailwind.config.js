19114d0339a53c1a6571115f5865739010d162f8
francoism90 <francoism90@users.noreply.github.com>
2024-03-04 18:43:47 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,10 @@ import theme from './resources/support/tailwind.config.preset';
 /** @type {import('tailwindcss').Config} */
 export default {
   presets: [preset, theme],
-  content: ['./resources/**/*.blade.php', './src/App/**/*.blade.php', './vendor/filament/**/*.blade.php', './vendor/foxws/livewire-use/**/*.blade.php'],
+  content: [
+    './resources/**/*.blade.php',
+    './src/App/**/*.blade.php',
+    './vendor/filament/**/*.blade.php',
+    './vendor/foxws/livewire-use/**/*.blade.php',
+  ],
 };
