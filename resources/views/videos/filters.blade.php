438e96098977ff53f6a5674e9525f8965f20022a
francoism90 <francoism90@users.noreply.github.com>
2024-03-05 13:55:15 +0100

    wip

--- resources/views/videos/filters.blade.php
+++ resources/views/videos/filters.blade.php
@@ -1,15 +1,15 @@
 <aside class="hidden w-72 min-w-72 max-w-72 flex-col gap-y-4 self-start sm:flex">
     @can('viewAny', Domain\Tags\Models\Tag::class)
         <div class="w-full rounded border border-gray-700/10 bg-gray-900/75 p-4">
-            <h3 class="headline pb-3.5 text-sm">{{ __('Tags') }}</h3>
+            <h3 class="font-medium uppercase text-primary-500 pb-3.5 text-sm">{{ __('Tags') }}</h3>
 
             <div class="flex max-h-96 flex-col gap-y-2 overflow-y-scroll">
                 @foreach ($ordered($this->form->tags) as $item)
                     <label
                         for="tag-{{ $item->getRouteKey() }}"
                         @class([
-                            'link text-sm uppercase',
-                            'link-active' => $this->form->hasTags($item),
+                            'uppercase text-sm hover:text-primary-400 cursor-pointer',
+                            'text-primary-400 hover:text-primary-300' => $this->form->hasTags($item),
                         ])
                     >
                         {{ $item->name }}
