5e8a08c0a058484f48ecb633a970f4760a02ecb8
francoism90 <francoism90@users.noreply.github.com>
2024-03-06 18:31:14 +0100

    wip

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -11,7 +11,7 @@
 
                 <div class="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
                     @foreach ($tags as $item)
-                        <x-tags-item :$item />
+                        <x-app::tags-item :$item />
                     @endforeach
                 </div>
             </article>
