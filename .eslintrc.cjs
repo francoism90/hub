3817c399c0a6dcbad3c1eb630805167c5bc20c64
francoism90 <francoism90@users.noreply.github.com>
2024-03-13 11:38:51 +0100

    Improve eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,24 +4,35 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: 'airbnb-base',
+  extends: [],
   overrides: [
     {
-      env: {
-        node: true,
-      },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script',
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx', '*.js'],
+      parser: '@typescript-eslint/parser',
+      extends: ['airbnb-base', 'plugin:tailwindcss/recommended'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+        'import/no-unresolved': 'off',
+        'tailwindcss/no-custom-classname': 'off',
+      },
+    },
+    {
+      files: ['*.html', '*.blade.php'],
+      parser: '@angular-eslint/template-parser',
+      extends: ['plugin:tailwindcss/recommended'],
+      rules: {
+        'tailwindcss/no-custom-classname': 'off',
       },
     },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-  },
-  rules: {
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-unresolved': 'off',
   },
 };
--- resources/views/components/app/header.blade.php
+++ resources/views/components/app/header.blade.php
@@ -7,7 +7,7 @@
                     class:layer="inline-flex items-center gap-x-4 text-2xl font-medium lowercase hover:text-primary-100"
                     class:active="text-inherit"
                 >
-                    <x-heroicon-s-play-circle class="h-12 w-12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-0.5" />
+                    <x-heroicon-s-play-circle class="size-12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-0.5" />
                     <span>{{ config('app.name') }}</span>
                 </x-livewire-use::actions-link>
             </x-slot:start>
@@ -19,7 +19,7 @@
                         aria-label="{{ __('Tags') }}"
                         title="{{ __('Tags') }}"
                     >
-                        <x-heroicon-o-hashtag class="h-6 w-6" />
+                        <x-heroicon-o-hashtag class="size-6" />
                     </x-livewire-use::actions-link>
 
                     <x-livewire-use::actions-link
@@ -27,19 +27,19 @@
                         aria-label="{{ __('Search') }}"
                         title="{{ __('Search') }}"
                     >
-                        <x-heroicon-o-magnifying-glass class="h-6 w-6" />
+                        <x-heroicon-o-magnifying-glass class="size-6" />
                     </x-livewire-use::actions-link>
 
                     <x-livewire-use::actions-dropdown>
                         <x-slot:actions>
                             <x-livewire-use::actions-button class:layer="block">
-                                <x-heroicon-o-user-circle class="h-6 w-6" />
+                                <x-heroicon-o-user-circle class="size-6" />
                             </x-livewire-use::actions-button>
                         </x-slot:actions>
 
                         <div
                             x-anchor.bottom-end.offset.5="$refs.dropdown"
-                            class="flex min-w-[16rem] max-w-[16rem] flex-col gap-y-4 rounded bg-gray-900 px-6 py-4"
+                            class="flex min-w-64 max-w-64 flex-col gap-y-4 rounded bg-gray-900 px-6 py-4"
                         >
                             <section class="flex flex-col flex-nowrap gap-y-1">
                                 <x-livewire-use::actions-link href="{{ route('profile.history') }}">
