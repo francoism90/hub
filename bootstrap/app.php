cd9d8b074f2eae788acf23eb70ee6d3c8c8a908f
francoism90 <francoism90@users.noreply.github.com>
2024-03-16 15:40:38 +0100

    Use app path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$basePath = $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__);
+
+$app = Application::configure(basePath: $basePath)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -25,3 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+$app->useAppPath($basePath.'/src/App');
+
+return $app;
