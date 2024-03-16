da6a0330320e316ffcfc32cd2451979116ffcb8e
francoism90 <francoism90@users.noreply.github.com>
2024-03-16 15:28:23 +0100

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'abilities' => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
+            'ability' => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
+            'cache' => \Spatie\ResponseCache\Middlewares\CacheResponse::class,
+            'cache.invalid' => \Spatie\ResponseCache\Middlewares\DoNotCacheResponse::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'private' => \Foundation\Http\Middlewares\IsPrivateSubnet::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
