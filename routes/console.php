ab424e787c3497284a6676a69f0c0f6d114194bc
francoism90 <francoism90@users.noreply.github.com>
2024-03-16 15:05:07 +0100

    Add providers

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,61 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+use Domain\Imports\Models\Import;
+use Foundation\Console\Commands\AppInstall;
+use Foundation\Console\Commands\AppOptimize;
+use Foundation\Console\Commands\AppUpdate;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Support\Scout\Commands\SyncIndexes;
+
+Artisan::registerCommand(AppInstall::class);
+Artisan::registerCommand(AppUpdate::class);
+Artisan::registerCommand(AppOptimize::class);
+Artisan::registerCommand(SyncIndexes::class);
+
+schedule::command('cache:prune-stale-tags')
+    ->withoutOverlapping(30)
+    ->hourly()
+    ->runInBackground();
+
+schedule::command('auth:clear-resets')
+    ->withoutOverlapping(600)
+    ->everyFifteenMinutes()
+    ->runInBackground();
+
+schedule::command('horizon:snapshot')
+    ->withoutOverlapping(240)
+    ->everyFiveMinutes()
+    ->runInBackground();
+
+schedule::command('sanctum:prune-expired --hours=24')
+    ->withoutOverlapping(1440)
+    ->dailyAt('01:30')
+    ->runInBackground();
+
+schedule::command('telescope:prune')
+    ->withoutOverlapping(1440)
+    ->dailyAt('02:00')
+    ->runInBackground();
+
+schedule::command('activitylog:clean')
+    ->withoutOverlapping(1440)
+    ->dailyAt('02:30')
+    ->runInBackground();
+
+schedule::command('snapshot:create')
+    ->withoutOverlapping(1440)
+    ->dailyAt('03:30')
+    ->runInBackground();
+
+schedule::command('snapshot:cleanup --keep=10')
+    ->withoutOverlapping(1440)
+    ->dailyAt('04:00')
+    ->runInBackground();
+
+schedule::command('model:prune', [
+        '--model' => [Import::class],
+    ])
+    ->withoutOverlapping(1440)
+    ->dailyAt('04:30')
+    ->runInBackground();
