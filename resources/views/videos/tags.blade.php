4f5d687a00b99bbc366130a5fd7e0d056100c4e7
francoism90 <francoism90@users.noreply.github.com>
2024-03-19 14:28:00 +0100

    wip

--- resources/views/videos/tags.blade.php
+++ resources/views/videos/tags.blade.php
@@ -1,7 +1,7 @@
 <div {{ $attributes->class('flex flex-wrap gap-x-2.5 gap-y-1 text-sm') }}>
     @foreach ($items as $item)
         <x-livewire-use::actions-link
-            wire:key="filter-{{ $item->getRouteKey() }}"
+            wire:key="query-{{ $item->getRouteKey() }}"
             class="font-medium uppercase tracking-tight text-primary-500 hover:text-primary-400"
             href="{{ route('home', ['t[]' => $item->getRouteKey()]) }}"
             aria-label="{{ $item->name }}"
