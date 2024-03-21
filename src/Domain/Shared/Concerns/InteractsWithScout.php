f29129daacebb5252d214a99c6784d79f9f39e19
francoism90 <francoism90@users.noreply.github.com>
2024-03-21 19:30:13 +0100

    Allow search parameters

--- src/Domain/Shared/Concerns/InteractsWithScout.php
+++ src/Domain/Shared/Concerns/InteractsWithScout.php
@@ -4,14 +4,15 @@ namespace Domain\Shared\Concerns;
 
 use Illuminate\Support\Collection;
 use Laravel\Scout\Builder;
+use Meilisearch\Endpoints\Indexes;
 
 trait InteractsWithScout
 {
-    public function search(string $value = '*', bool $scopes = false): self
+    public function search(string $value = '*', array $options = [], bool $scopes = false): self
     {
         $column = $this->getTableColumn();
 
-        $keys = $this->getScoutKeys($value, $scopes);
+        $keys = $this->getScoutKeys($value, $options, $scopes);
 
         return $this
             ->reorder()
@@ -19,11 +20,15 @@ trait InteractsWithScout
             ->orderByRaw("FIND_IN_SET ({$column}, ?)", [$keys->implode(',')]);
     }
 
-    protected function getScoutKeys(string $value = '*', bool $scopes = false): Collection
+    protected function getScoutKeys(string $value = '*', array $parameters = [], bool $scopes = false): Collection
     {
         return $this
             ->getModel()
-            ->search($value)
+            ->search($value, function (Indexes $engine, string $query, array $options) use ($parameters) {
+                $options = array_merge_recursive($options, $parameters);
+
+                return $engine->search($query, $options);
+            })
             ->when($scopes, fn (Builder $query) => $query->whereIn('id', $this->getModelKeys()))
             ->take($this->getScoutLimit())
             ->keys();
