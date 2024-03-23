ef7d7fbc813bf1aa65dfc735a66a54de1c5928b8
francoism90 <francoism90@users.noreply.github.com>
2024-03-23 10:31:04 +0100

    wip

--- resources/views/tags/view.blade.php
+++ resources/views/tags/view.blade.php
@@ -3,16 +3,6 @@
         class="grow flex-col gap-y-4 divide-y divide-gray-700 overflow-hidden"
         wire:poll.keep-alive.2400s
     >
-        {{-- @if ($form->filled('search', 'tags'))
-            <x-livewire-use::actions-button
-                class="btn-ghost m-1.5 p-1.5 text-gray-400"
-                wire:click="clear"
-            >
-                <x-heroicon-o-magnifying-glass-minus class="h-5" />
-                <span>{{ __('Reset Filters') }}</span>
-            </x-livewire-use::actions-button>
-        @endif --}}
-
         @forelse ($this->items as $item)
             <x-app::videos-card :$item />
         @empty
@@ -25,6 +15,4 @@
             {{ $this->items->links('pagination.simple') }}
         </div>
     </div>
-
-    {{-- <x-app::videos-filters /> --}}
 </x-livewire-use::layout-container>
