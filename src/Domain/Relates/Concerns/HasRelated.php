420eed7b2472571726b94525f934b6fbac689c20
francoism90 <francoism90@users.noreply.github.com>
2024-03-27 10:25:07 +0100

    Only fetch taggables

--- src/Domain/Relates/Concerns/HasRelated.php
+++ src/Domain/Relates/Concerns/HasRelated.php
@@ -3,7 +3,6 @@
 namespace Domain\Relates\Concerns;
 
 use Domain\Relates\Models\Relatable;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait HasRelated
@@ -11,16 +10,5 @@ trait HasRelated
     public function relatables(): MorphMany
     {
         return $this->morphMany(Relatable::class, 'model');
-    }
-
-    protected function relates(): Attribute
-    {
-        return Attribute::make(
-            get: fn () => $this
-                ->relatables()
-                ->scores()
-                ->get()
-                ->relates()
-        )->shouldCache();
     }
 }
