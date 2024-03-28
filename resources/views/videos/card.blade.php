e84a5f00518646f15f6e746f1fb07b0b85e07371
francoism90 <francoism90@users.noreply.github.com>
2024-03-28 14:12:48 +0100

    wip

--- resources/views/videos/card.blade.php
+++ resources/views/videos/card.blade.php
@@ -5,7 +5,7 @@
 >
     <div class="flex flex-wrap flex-grow md:flex-nowrap gap-6">
         <x-livewire-use::actions-link
-            class="grow h-64 w-full md:w-96 md:max-w-96 bg-black"
+            class="grow h-64 w-full md:w-96 md:max-w-96"
             href="{{ route('videos.view', $item) }}"
         >
             <figure
@@ -16,37 +16,47 @@
                 x-on:touchend.passive="preview = false"
                 class="relative h-full w-full md:w-96 md:max-w-96 bg-black"
             >
-                    <img
-                        alt="{{ $item->title }}"
-                        src="{{ $item->thumbnail }}"
-                        srcset="{{ $item->placeholder }}"
-                        class="absolute inset-0 z-0 h-full w-full object-fill"
-                        crossorigin="use-credentials"
-                        loading="lazy"
+                <img
+                    alt="{{ $item->title }}"
+                    src="{{ $item->thumbnail }}"
+                    srcset="{{ $item->placeholder }}"
+                    class="absolute inset-0 z-0 h-full w-full object-fill"
+                    crossorigin="use-credentials"
+                    loading="lazy"
+                />
+
+                <div class="absolute inset-0 z-20 h-full w-full">
+                    <dl class="absolute z-30 bottom-2 right-2 flex items-center gap-x-1.5 bg-black/30 px-1 py-0.5 text-xs font-medium text-gray-300">
+                        @if ($item->duration)
+                            <dt class="sr-only">{{ __('Duration') }}</dt>
+                            <dd>
+                                <time datetime="{{ $item->duration }}">
+                                    {{ duration($item->duration) }}
+                                </time>
+                            </dd>
+                        @endif
+
+                        @if ($item->caption)
+                            <dt class="sr-only">{{ __('Caption') }}</dt>
+                            <dd>
+                                {{ __('CC' )}}
+                            </dd>
+                        @endif
+                    </dl>
+                </div>
+
+                <template x-if="preview">
+                    <x-app::videos-player
+                        :$item
+                        :manifest="$item->preview"
+                        :controls="false"
+                        :rate="1.05"
+                        class="absolute inset-0 z-10 h-full w-full object-fill"
+                        autoplay
+                        muted
+                        loop
                     />
-
-                    <div class="absolute inset-0 z-20 h-full w-full">
-                        <div class="z-30 absolute bottom-2 right-2 flex items-center gap-x-1.5 bg-black/30 px-1 py-0.5 text-xs font-medium text-gray-300">
-                            @if ($item->caption)
-                                <span>{{ __('CC') }}</span>
-                            @endif
-
-                            <span>{{ duration($item->duration) }}</span>
-                        </div>
-                    </div>
-
-                    <template x-if="preview">
-                        <x-app::videos-player
-                            :$item
-                            :manifest="$item->preview"
-                            :controls="false"
-                            :rate="1.05"
-                            class="absolute inset-0 z-10 h-full w-full object-fill"
-                            autoplay
-                            muted
-                            loop
-                        />
-                    </template>
+                </template>
             </figure>
         </x-livewire-use::actions-link>
 
@@ -62,12 +72,14 @@
                 </x-livewire-use::actions-link>
 
                 <dl class="inline-flex items-center">
-                    <dt class="sr-only">{{ __('Published on') }}</dt>
-                    <dd class="text-base font-medium leading-4 text-gray-400">
-                        <time datetime="{{ $item->published->format('Y-m-d\TH:i:s.uP') }}">
-                            {{ $item->published->format('F d, Y') }}
-                        </time>
-                    </dd>
+                    @if ($item->published)
+                        <dt class="sr-only">{{ __('Published on') }}</dt>
+                        <dd class="text-base font-medium leading-4 text-gray-400">
+                            <time datetime="{{ $item->published->format('Y-m-d\TH:i:s.uP') }}">
+                                {{ $item->published->format('F d, Y') }}
+                            </time>
+                        </dd>
+                    @endif
 
                     @if ($item->identifier)
                         <dt class="sr-only">{{ __('ID') }}</dt>
@@ -78,7 +90,7 @@
                 </dl>
 
                 @if ($item->summary)
-                    <p class="prose prose-slate"">
+                    <p class="prose prose-slate line-clamp-4"">
                         {{ $item->summary }}
                     </p>
                 @endif
