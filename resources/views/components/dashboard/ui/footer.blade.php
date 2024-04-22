b4f017efa5bb01a40af2d298617430ff46c9cebf
francoism90 <francoism90@users.noreply.github.com>
2024-04-22 16:29:30 +0200

    wip

--- resources/views/components/dashboard/ui/footer.blade.php
+++ resources/views/components/dashboard/ui/footer.blade.php
@@ -1,23 +1,17 @@
 <footer class="border-t border-secondary-800/80">
     <x-wireui::layout-container class="flex items-center justify-center py-1.5 gap-6">
-        <x-wireui::actions-link
-            route="tags.index"
-            aria-label="{{ __('Tags') }}"
-            title="{{ __('Tags') }}"
-            class="flex-col text-xs gap-1 line-clamp-1"
-        >
-            <x-heroicon-o-rectangle-stack class="size-6" />
-            <span>Tags</span>
-        </x-wireui::actions-link>
+        @foreach ($items->all() as $item)
+            <x-wireui::actions-link
+                route="{{ $item->route }}"
+                aria-label="{{ $item->name }}"
+                title="{{ $item->name }}"
+                class="flex-col text-xs gap-1 line-clamp-1 max-w-24"
+            >
+                {{ $component->getUrl() }}
 
-        <x-wireui::actions-link
-            route="tags.index"
-            aria-label="{{ __('Tags') }}"
-            title="{{ __('Tags') }}"
-            class="flex-col text-xs gap-1 line-clamp-1"
-        >
-            <x-heroicon-o-rectangle-stack class="size-6" />
-            <span>Tags</span>
+                <x-icon name="{{ $item->icon }}" class="size-6" />
+                <span>{{ $item->name }}</span>
         </x-wireui::actions-link>
+        @endforeach
     </x-wireui::layout-container>
 </footer>
--- src/App/View/Components/Dashboard/Ui/Footer.php
+++ src/App/View/Components/Dashboard/Ui/Footer.php
@@ -3,6 +3,8 @@
 namespace App\View\Components\Dashboard\Ui;
 
 use Closure;
+use Foxws\WireUse\Menus\Support\MenuGroup;
+use Foxws\WireUse\Menus\Support\MenuItem;
 use Foxws\WireUse\Views\Support\Component;
 use Illuminate\Contracts\View\View;
 
@@ -11,5 +13,28 @@ class Footer extends Component
     public function render(): View|Closure|string
     {
         return view('components.dashboard.ui.footer');
+    }
+
+    public function items(): MenuGroup
+    {
+        return MenuGroup::make([
+            MenuItem::make()
+                ->name(__('Dashboard'))
+                ->route('tags.index')
+                ->icon('heroicon-o-squares-2x2')
+                ->activeIcon('heroicon-s-squares-2x2'),
+
+            MenuItem::make()
+                ->name(__('Content'))
+                ->route('tags.index')
+                ->icon('heroicon-o-rectangle-stack')
+                ->activeIcon('heroicon-s-squares-2x2'),
+
+            MenuItem::make()
+                ->name(__('Analytics'))
+                ->route('tags.index')
+                ->icon('heroicon-o-rectangle-stack')
+                ->activeIcon('heroicon-s-squares-2x2'),
+        ]);
     }
 }
