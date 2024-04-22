8d655603b1f7780436eab11e02e94f3d6260d694
francoism90 <francoism90@users.noreply.github.com>
2024-04-22 19:09:16 +0200

    wip

--- resources/views/components/app/ui/logo.blade.php
+++ resources/views/components/app/ui/logo.blade.php
@@ -1,4 +1,4 @@
 <div class="inline-flex items-center gap-x-4 text-2xl font-medium lowercase hover:text-primary-100">
-    <x-heroicon-s-play-circle class="size-12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-0.5" />
+    <x-heroicon-s-play-circle class="size-10 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-0.5" />
     <span>{{ config('app.name') }}</span>
 </div>
--- resources/views/components/dashboard/ui/header.blade.php
+++ resources/views/components/dashboard/ui/header.blade.php
@@ -1,5 +1,5 @@
 <header class="border-b border-secondary-800/80">
-    <x-wireui::navigation-navbar class:padding="py-2 px-6">
+    <x-wireui::navigation-navbar class:padding="p-3">
         <x-slot:start>
             <x-wireuse::actions-link :action="$homeUrl()">
                 <x-app.ui.logo />
