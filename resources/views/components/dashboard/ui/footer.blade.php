3b03d78ffdac9236616deb99e6f2948d8f3962d7
francoism90 <francoism90@users.noreply.github.com>
2024-04-22 18:48:02 +0200

    wip

--- resources/views/components/dashboard/ui/footer.blade.php
+++ resources/views/components/dashboard/ui/footer.blade.php
@@ -1,9 +1,8 @@
 <footer class="border-t border-secondary-800/80">
     <x-wireui::layout-container class="flex items-center justify-center py-1.5 gap-6">
         @foreach ($actions->all() as $action)
-            <x-wireuse::actions-link :$action
-                aria-label="{{ $action->getName() }}"
-                title="{{ $action->name }}"
+            <x-wireuse::actions-link
+                :$action
                 class="flex-col text-xs gap-1 line-clamp-1"
             >
                 <x-icon :name="$component->icon()" class="size-6" />
