d978e10b9ace9f1a7ba5eba63d0c1501c780e214
francoism90 <francoism90@users.noreply.github.com>
2024-04-23 17:57:45 +0200

    wip

--- resources/views/livewire/dashboard/pages/content.blade.php
+++ resources/views/livewire/dashboard/pages/content.blade.php
@@ -1,3 +1,6 @@
 <x-wireuse::layout-page>
-    <x-wireuse::navigation-tabs :navigation="$state->navigation()" />
+    <x-wireuse::navigation-tabs
+        :navigation="$state->navigation()"
+        class="px-3 gap-5 border-b border-secondary-800/80"
+    />
 </x-wireuse::layout-page>
--- resources/views/livewire/dashboard/ui/footer.blade.php
+++ resources/views/livewire/dashboard/ui/footer.blade.php
@@ -1,5 +1,8 @@
 <footer class="border-t border-secondary-800/80">
     <x-wireuse::layout-container class="flex items-center justify-center py-1.5 gap-6 sm:gap-9 overflow-x-auto">
-        <x-wireuse::navigation-tabs :navigation="$state->navigation()" />
+        <x-wireuse::navigation-tabs
+            :navigation="$state->navigation()"
+            class="px-3 gap-5"
+        />
     </x-wireuse::layout-container>
 </footer>
