2b8dd087150e181fdd086be2eb4a0c7de4b88aad
francoism90 <francoism90@users.noreply.github.com>
2024-04-24 15:04:08 +0200

    wip

--- resources/views/livewire/dashboard/pages/content.blade.php
+++ resources/views/livewire/dashboard/pages/content.blade.php
@@ -1,3 +1,10 @@
 <x-wireuse::layout-page>
-    <x-wireuse::navigation-tabs :$navigation />
+    <x-wireuse::navigation-tabs
+        :$navigation
+        class="px-3 gap-5 border-b border-secondary-500/50 w-full"
+        class:tab="py-3 border-b text-sm font-medium"
+        class:active="border-primary-400 text-primary-400"
+        class:inactive="border-secondary-400 text-secondary-400"
+
+    />
 </x-wireuse::layout-page>
--- resources/views/livewire/dashboard/ui/header.blade.php
+++ resources/views/livewire/dashboard/ui/header.blade.php
@@ -1,9 +1,9 @@
 <header class="border-b border-secondary-800/80">
-    <x-wireui::navigation-navbar class:padding="p-3">
+    <x-wireuse::navigation-navbar class:padding="p-3">
         <x-slot:start>
             <a href="/" wire:navigate>
                 <x-app.ui.logo />
             </a>
         </x-slot:start>
-    </x-wireui::navigation-navbar>
+    </x-wireuse::navigation-navbar>
 </header>
