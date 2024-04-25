605b748f5c64191cf71ad3dda13619bb37c9a0b4
francoism90 <francoism90@users.noreply.github.com>
2024-04-25 16:28:47 +0200

    wip

--- src/App/Livewire/Dashboard/Videos/Forms/QueryForm.php
+++ src/App/Livewire/Dashboard/Videos/Forms/QueryForm.php
@@ -11,13 +11,13 @@ class QueryForm extends Form
 
     protected static bool $store = true;
 
-    #[Validate('nullable|string|max:255')]
-    public string $search = '';
-
     #[Validate('required|string|in:recent,random|max:255')]
     public string $sort = 'recent';
 
-    #[Validate('required|array|in:verified,pending,failed')]
+    #[Validate('nullable|string|max:255')]
+    public string $search = '';
+
+    #[Validate('nullable|array|in:verified,pending,failed')]
     public array $visibility = [];
 
     public function getSearch(): string
