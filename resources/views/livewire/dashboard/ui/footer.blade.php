7e218824e9380b8eb7acc1be2b77250f91f4965e
francoism90 <francoism90@users.noreply.github.com>
2024-04-25 16:58:43 +0200

    wip

--- resources/views/livewire/dashboard/ui/footer.blade.php
+++ resources/views/livewire/dashboard/ui/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="sticky bottom-0 z-30 py-1.5 px-3 bg-inherit border-t border-secondary-800/80">
-    <x-wireuse::layout-container class="flex items-center justify-center">
+    <x-wireuse::layout-container fluid>
         <x-wireuse::navigation-tabs
             :$navigation
-            class:tabs="flex items-center overflow-x-auto gap-x-6"
-            class:item="flex-col gap-0.5 justify-stretch max-w-16 line-clamp text-xs font-medium"
+            class:tabs="flex items-center justify-between gap-x-6 overflow-x-auto sm:justify-center"
+            class:item="flex flex-col max-w-16 gap-0.5 justify-stretch line-clamp text-xs font-medium"
             class:icon="size-6 sm:size-7"
         />
     </x-wireuse::layout-container>
