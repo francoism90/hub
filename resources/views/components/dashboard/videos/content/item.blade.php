bf413b4fe0ae873ef4de824d7e3ff86b65437f7f
francoism90 <francoism90@users.noreply.github.com>
2024-04-25 16:07:47 +0200

    wip

--- resources/views/components/dashboard/videos/content/item.blade.php
+++ resources/views/components/dashboard/videos/content/item.blade.php
@@ -4,65 +4,58 @@
 
 <article
     wire:key="item-{{ $item->getRouteKey() }}"
-    class="flex flex-col flex-nowrap h-72 max-h-72 w-full gap-x-4"
+    class="flex flex-col flex-nowrap h-72 max-h-72 w-full"
 >
     <x-dashboard.videos.content.preview />
 
-    <div class="flex flex-col p-3">
-            <a href="{{ route('videos.view', $item) }}">
-                <h2
-                    class="line-clamp-1 text-sm font-medium capitalize tracking-tight"
-                    aria-label="{{ $item->title }}"
-                    title="{{ $item->title }}"
-                >
-                    {{ $item->title }}
-                </h2>
+    <a class="block" href="{{ route('videos.view', $item) }}">
+        <h1 class="pt-3 font-medium text-sm leading-none tracking-tight line-clamp-2">
+            {{ $item->title }}
+        </h1>
 
-                <dl>
-                    <dt class="sr-only">{{ __('Time') }}</dt>
-                    <dd class="text-ellipsis text-xs font-medium text-gray-400">
-                        <time>
-                            {{ duration($item->duration) }}
-                        </time>
-                    </dd>
+        <dl class="inline-flex items-center text-xs text-secondary-400">
+            <dt class="sr-only">{{ __('Time') }}</dt>
+            <dd class="text-ellipsis">
+                <time>
+                    {{ duration($item->duration) }}
+                </time>
+            </dd>
 
-                    @if ($item->episode || $item->season)
-                        <dt class="sr-only">{{ __('ID') }}</dt>
-                        <dd class="text-xs font-medium text-gray-400">
-                            {{ $item->identifier }}
-                        </dd>
-                    @endif
-
-                    @if ($item->caption)
-                        <dt class="sr-only">{{ __('Captions') }}</dt>
-                        <dd class="text-xs font-medium text-gray-400">
-                            {{ __('CC') }}
-                        </dd>
-                    @endif
-
-                    <dt class="sr-only">{{ __('Published on') }}</dt>
-                    <dd class="text-ellipsis text-xs font-medium text-gray-400">
-                        <time datetime="{{ $item->published->format('Y-m-d\TH:i:s.uP') }}">
-                            {{ $item->published->format('M d, Y') }}
-                        </time>
-                    </dd>
-                </dl>
-            </a>
-
-            @if ($item->tags()->count())
-                <div class="flex flex-wrap gap-2">
-                    @foreach ($item->tags as $tag)
-                        <a
-                            wire:key="tag-{{ $tag->getRouteKey() }}"
-                            class="text-xs font-medium uppercase tracking-tight text-primary-500 hover:text-primary-400"
-                            href="{{ route('tags.view', $tag) }}"
-                            aria-label="{{ $tag->name }}"
-                            title="{{ $tag->name }}"
-                        >
-                            {{ $tag->name }}
-                        </a>
-                    @endforeach
-                </div>
+            @if ($item->episode || $item->season)
+                <dt class="sr-only">{{ __('ID') }}</dt>
+                <dd class="text-ellipsis">
+                    {{ $item->identifier }}
+                </dd>
             @endif
+
+            @if ($item->caption)
+                <dt class="sr-only">{{ __('Captions') }}</dt>
+                <dd class="text-ellipsis">
+                    {{ __('CC') }}
+                </dd>
+            @endif
+
+            <dt class="sr-only">{{ __('Published on') }}</dt>
+            <dd class="text-ellipsis">
+                <time datetime="{{ $item->published->jsonSerialize() }}">
+                    {{ $item->published->format('M d, Y') }}
+                </time>
+            </dd>
+        </dl>
+    </a>
+
+    @if ($item->tags()->count())
+        <div class="flex flex-wrap gap-2">
+            @foreach ($item->tags as $tag)
+                <a
+                    wire:key="tag-{{ $tag->getRouteKey() }}"
+                    class="text-xs font-medium uppercase tracking-tight text-primary-500 hover:text-primary-400"
+                    href="{{ route('tags.view', $tag) }}"
+                    aria-label="{{ $tag->name }}"
+                >
+                    {{ $tag->name }}
+                </a>
+            @endforeach
         </div>
+    @endif
 </article>
