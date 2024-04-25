e435fb0b91b20e2b5ff030e221b44c50c45ad5f7
francoism90 <francoism90@users.noreply.github.com>
2024-04-25 22:00:45 +0200

    wip

--- resources/views/components/app/videos/feed/item.blade.php
+++ resources/views/components/app/videos/feed/item.blade.php
@@ -17,7 +17,7 @@
         loading="lazy"
     />
 
-    <div class="absolute z-10 mx-auto inset-0 w-3/5">
+    <div class="absolute z-10 mx-auto inset-0 w-full sm:w-3/5 xl:max-w-2xl">
         <x-app.videos.feed.preview />
         <x-app.videos.feed.details />
     </div>
