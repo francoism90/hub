e731334a3215b1c037c90b5afa2045485b319433
francoism90 <francoism90@users.noreply.github.com>
2024-04-26 16:51:17 +0200

    wip

--- resources/views/livewire/dashboard/pages/content.blade.php
+++ resources/views/livewire/dashboard/pages/content.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-wireuse::navigation-tabs
         :$navigation
-        class:tabs="flex items-center overflow-x-auto gap-x-5 px-3 border-b border-secondary-500/50"
+        class:layer="flex items-center overflow-x-auto gap-x-5 px-3 border-b border-secondary-500/50"
         class:item="py-3 border-b text-sm font-medium"
         class:active="border-primary-400 text-primary-400"
         class:inactive="border-secondary-400 text-secondary-400"
