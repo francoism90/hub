205cb2861a84b0547d62c76081cf4b71d43d7900
francoism90 <francoism90@users.noreply.github.com>
2024-04-26 16:48:05 +0200

    wip

--- resources/views/tags/item.blade.php
+++
@@ -1,8 +0,0 @@
-<x-wireui::actions-link
-    wire:key="item-{{ $item->getRouteKey() }}"
-    class="block cursor-pointer text-sm font-medium uppercase"
-    {{-- href="{{ route('tags.view', $item) }}" --}}
->
-    <span class="text-primary-500 hover:text-primary-400">{{ $item->name }}</span>
-    <span>({{ $item->videos_count }})</span>
-</x-wireui::actions-link>
