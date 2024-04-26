cd87899491ad1c019198fdfd349ea802f08cdc47
francoism90 <francoism90@users.noreply.github.com>
2024-04-26 18:46:30 +0200

    wip

--- resources/views/components/app/videos/feed/preview.blade.php
+++ resources/views/components/app/videos/feed/preview.blade.php
@@ -3,36 +3,41 @@
     'preview',
 ])
 
-<div x-data="player('{{ $video->preview }}')">
-    <div
-        x-ref="container"
-        x-intersect:leave="destroy"
-        class="absolute z-20 inset-y-0 inset-x-0 bg-black/25 sm:inset-x-10 sm:bg-black"
-    >
-        <img
-            alt="{{ $video->title }}"
-            srcset="{{ $video->placeholder }}"
-            src="{{ $video->thumbnail }}"
-            class="h-full w-full brightness-90 object-contain"
-            crossorigin="use-credentials"
-            loading="lazy"
-        />
+<a
+    wire:navigate
+    href="{{ route('videos.view', $video) }}"
+>
+    <div x-data="player('{{ $video->preview }}')">
+        <div
+            x-ref="container"
+            x-intersect:leave="destroy"
+            class="absolute z-20 inset-y-0 inset-x-0 bg-black/25 sm:inset-x-10 sm:bg-black"
+        >
+            <img
+                alt="{{ $video->title }}"
+                srcset="{{ $video->placeholder }}"
+                src="{{ $video->thumbnail }}"
+                class="h-full w-full brightness-90 object-contain"
+                crossorigin="use-credentials"
+                loading="lazy"
+            />
 
-        <video
-            x-cloak
-            x-ref="video"
-            x-show="ready"
-            playsinline
-            muted
-            autoplay
-            loop
-            @class([
-                'h-full w-full absolute inset-0 z-40 brightness-90' => $preview,
-                'hidden' => ! $preview,
-            ])
-        />
+            <video
+                x-cloak
+                x-ref="video"
+                x-show="ready"
+                playsinline
+                muted
+                autoplay
+                loop
+                @class([
+                    'h-full w-full absolute inset-0 z-40 brightness-90' => $preview,
+                    'hidden' => ! $preview,
+                ])
+            />
+        </div>
     </div>
-</div>
+</a>
 
 @script
     <script>
