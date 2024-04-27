fd6bd8ff0b4a2230aa21a448f6981c9952249c7a
francoism90 <francoism90@users.noreply.github.com>
2024-04-27 09:58:29 +0200

    wip

--- src/App/Livewire/Player/Video.php
+++ src/App/Livewire/Player/Video.php
@@ -3,6 +3,8 @@
 namespace App\Livewire\Player;
 
 use App\Livewire\App\Videos\Concerns\WithVideo;
+use Foxws\WireUse\Navigation\Support\Navigation;
+use Foxws\WireUse\Navigation\Support\NavigationItem;
 use Illuminate\View\View;
 use Livewire\Component;
 
@@ -13,7 +15,42 @@ class Video extends Component
     public function render(): View
     {
         return view('livewire.app.player.video')->with([
-            // 'controls' => $this->controls(),
+            'controls' => $this->controls(),
         ]);
+    }
+
+    protected function controls(): Navigation
+    {
+        return Navigation::make()
+            ->add('home', fn (NavigationItem $item) => $item
+                ->label(__('Feed'))
+                ->icon('heroicon-o-square-2-stack')
+                ->iconActive('heroicon-s-square-2-stack')
+                ->route('home')
+            )
+            ->add('discover', fn (NavigationItem $item) => $item
+                ->label(__('Discover'))
+                ->icon('heroicon-o-magnifying-glass')
+                ->iconActive('heroicon-s-magnifying-glass')
+                ->route('dashboard.content')
+            )
+            ->add('settings', fn (NavigationItem $item) => $item
+                ->label(__('Collections'))
+                ->icon('heroicon-o-bookmark')
+                ->iconActive('heroicon-s-bookmark')
+                ->route('dashboard.index')
+            )
+            ->add('activity', fn (NavigationItem $item) => $item
+                ->label(__('Account'))
+                ->icon('heroicon-o-user')
+                ->iconActive('heroicon-s-user')
+                ->route('dashboard.index')
+            )
+            ->add('post', fn (NavigationItem $item) => $item
+                ->label(__('More'))
+                ->icon('heroicon-o-ellipsis-horizontal')
+                ->iconActive('heroicon-s-ellipsis-horizontal')
+                ->route('dashboard.index')
+            );
     }
 }
