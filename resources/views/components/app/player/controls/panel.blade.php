4adbdde1b95442fa0e4a9859c340636d9707f8c3
francoism90 <francoism90@users.noreply.github.com>
2024-04-27 16:53:16 +0200

    wip

--- resources/views/components/app/player/controls/panel.blade.php
+++ resources/views/components/app/player/controls/panel.blade.php
@@ -3,14 +3,12 @@
     'controls'
 ])
 
-<div class="absolute z-10 bottom-8 left-6">
+<div class="absolute z-10 bottom-4 left-6">
     <div class="flex flex-row flex-nowrap items-center gap-3">
         <x-wireuse::actions-group
             :group="$controls"
-            {{-- class:layer="flex items-center overflow-x-auto gap-x-5 px-3 border-b border-secondary-500/50" --}}
-            {{-- class:item="py-3 border-b text-sm font-medium" --}}
-            {{-- class:active="border-primary-400 text-primary-400" --}}
-            {{-- class:inactive="border-secondary-400 text-secondary-400" --}}
+            class:label="sr-only"
+            class:icon="size-6 sm:size-7"
         />
 
         <p class="text-sm font-medium">
