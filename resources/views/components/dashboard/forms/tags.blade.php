28ad20584477d00e0d65282f312de902393ce516
francoism90 <francoism90@users.noreply.github.com>
2024-04-28 22:55:39 +0200

    wip

--- resources/views/components/dashboard/forms/tags.blade.php
+++ resources/views/components/dashboard/forms/tags.blade.php
@@ -7,11 +7,7 @@
     $tags = $items->select(['name', 'prefixed_id'])->toArray();
 @endphp
 
-<div
-    x-data="{ tags: $wire.entangle('{{ $id }}'), map: {{ Js::from($tags) }} }"
-    x-init="$watch('map', value => tags = value)"
-
-    {{ $attributes
+<div {{ $attributes
         ->cssClass([
             'layer' => 'flex items-start gap-2 p-3 min-h-24 max-h-32 w-full bg-secondary-800/90 border border-secondary-500/50 text-base focus:border-secondary-500 focus:border-2 focus:ring-0',
             'error' => '!border-red-500',
@@ -21,23 +17,31 @@
             'error' => $errors->has($id),
         ])
         ->merge([
+            'x-data' => 'tags',
             'id' => $id,
         ])
-    }}
->
-    <template x-for="(item, index) in map" :key="item.prefixed_id">
+}}>
+    <template x-for="(tag, index) in tags" :key="tag.prefixed_id">
         <a
-            x-text="item.name"
-            x-on:click="map.splice(index, 1)"
+            x-text="tag.name"
+            x-on:click="tags.splice(index, 1)"
         >
         </a>
     </template>
-
-        {{-- <a
-            wire:click="$dispatchTo('form', 'show-post-modal')"
-            class="inline-flex items-center gap-1 text-sm font-medium px-3 h-7 bg-primary-500/50"
-        >
-            {{ $item->name }}
-            <x-icon name="heroicon-c-x-circle" class="size-3" />
-        </a> --}}
 </div>
+
+@script
+    <script data-navigate-track>
+        Alpine.data('tags', () => ({
+            selected: $wire.entangle('{{ $id }}'),
+            tags: {{ Js::from($tags) }},
+
+            async init() {
+                this.$watch('tags', (value) => {
+                    values = JSON.parse(JSON.stringify(value))
+                    this.selected = values.map(obj => obj.prefixed_id)
+                })
+            },
+        }));
+    </script>
+@endscript
