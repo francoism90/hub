2a188bf114b900b40ea487901ced1a8180a40ffa
francoism90 <francoism90@users.noreply.github.com>
2024-04-28 09:15:02 +0200

    wip

--- resources/views/livewire/app/pages/videos/view.blade.php
+++ resources/views/livewire/app/pages/videos/view.blade.php
@@ -1,7 +1,10 @@
-@props([
+{{-- @props([
     'video',
 ])
 
 <div class="relative h-full w-full">
     <livewire:livewire.player.video :$video :key="$video->getRouteKey()" />
+</div> --}}
+
+<div>
 </div>
