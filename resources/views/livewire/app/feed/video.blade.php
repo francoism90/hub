08aa6c820e890bfab3e12e4f33f24adc14840ee1
francoism90 <francoism90@users.noreply.github.com>
2024-04-28 16:42:23 +0200

    wip

--- resources/views/livewire/app/feed/video.blade.php
+++ resources/views/livewire/app/feed/video.blade.php
@@ -3,7 +3,7 @@
         alt="{{ $video->title }}"
         srcset="{{ $video->placeholder }}"
         src="{{ $video->thumbnail }}"
-        class="absolute z-0 inset-0 object-fill blur-3xl brightness-50 saturate-50"
+        class="absolute z-0 inset-0 h-full w-full object-fill blur-3xl brightness-50 saturate-50"
         crossorigin="use-credentials"
         loading="lazy"
     />
