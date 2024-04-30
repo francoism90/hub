c9831ab4c1478aa4f825450c597b6433244a1a54
francoism90 <francoism90@users.noreply.github.com>
2024-04-30 14:28:18 +0200

    wip

--- resources/views/livewire/dashboard/pages/content/index.blade.php
+++ resources/views/livewire/dashboard/pages/content/index.blade.php
@@ -1,8 +1,5 @@
 <div>
-    <x-wireuse::navigation-tabs
-        :nodes="$this->getNodes()"
-        wire:model.live="tab"
-    />
+    <x-wireuse::navigation-tabs :nodes="$this->getNodes()" wire:model.live="tab" />
 
     {{ $tab }}
 </div>
