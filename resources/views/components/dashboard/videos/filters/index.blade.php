71c6024b7a9f88f3848f51a97fb864db4ec6fe0b
francoism90 <francoism90@users.noreply.github.com>
2024-05-14 14:49:08 +0200

    wip

--- resources/views/components/dashboard/videos/filters/index.blade.php
+++ resources/views/components/dashboard/videos/filters/index.blade.php
@@ -4,7 +4,7 @@
 
 <div
     x-data
-    class="flex h-8 max-h-8 w-full gap-x-2 overflow-y-hidden overflow-x-scroll"
+    class="flex flex-nowrap items-center gap-x-2"
 >
     @foreach($actions as $action)
         <x-dynamic-component :component="$action->getComponent()" :$action />
