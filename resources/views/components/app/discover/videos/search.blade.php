9d1f0c9f174bd8c0613d70030dd49dc37a09471a
francoism90 <francoism90@users.noreply.github.com>
2024-05-17 20:07:29 +0200

    Add backspace

--- resources/views/components/app/discover/videos/search.blade.php
+++ resources/views/components/app/discover/videos/search.blade.php
@@ -1,10 +1,19 @@
 <header class="sticky top-0 z-30 h-16 w-full border-b border-secondary-800/80 bg-secondary-950">
-    <x-app.navigation.navbar>
-        <x-dashboard.forms.input
-            placeholder="{{ __('Type something..') }}"
-            id="form.query"
-            class:input="px-3 h-16 w-screen max-w-screen text-base bg-transparent border-0 focus:border-0 focus:ring-0"
-            wire:model.live.debounce.300ms="form.query"
-        />
-    </x-app.navigation.navbar>
+    <x-dashboard.forms.input
+        placeholder="{{ __('Type something..') }}"
+        id="form.query"
+        class:input="px-3 h-16 w-screen max-w-screen text-base bg-transparent border-0 focus:border-0 focus:ring-0"
+        wire:model.live.debounce.300ms="form.query"
+    >
+    @if ($this->form->query())
+        <x-slot:append>
+            <a
+                class="p-3"
+                wire:click="$set('form.query', '')"
+            >
+                <x-heroicon-m-backspace class="size-5 fill-secondary-500" />
+            </a>
+        </x-slot:append>
+    @endif
+    </x-dashboard.forms.input>
 </header>
