df7b032ae79f951f9b2bca15fc5dc2088d470b3f
francoism90 <francoism90@users.noreply.github.com>
2024-05-18 12:18:51 +0200

    wip

--- resources/views/livewire/dashboard/content/tags.blade.php
+++ resources/views/livewire/dashboard/content/tags.blade.php
@@ -6,22 +6,24 @@
         wire:poll.visible.900s
     >
         @forelse ($this->items as $item)
-            <article class="flex h-12 max-h-12 gap-3 w-full">
-                <x-heroicon-s-tag class="bg-gradient-to-tl from-indigo-500 via-purple-500 to-primary-500 p-3" />
+            <a class="block">
+                <article class="flex h-12 max-h-12 gap-3 w-full">
+                    <x-heroicon-s-tag class="bg-gradient-to-tl from-indigo-500 via-purple-500 to-primary-500 p-3" />
 
-                <div class="flex flex-col w-full">
-                    <h1 class="text-base font-semibold">{{ $item->name }}</h1>
-                    <dl class="dl text-sm text-secondary-300">
-                        @if ($item->type)
-                            <dt class="sr-only">{{ __('Type') }}</dt>
-                            <dd class="text-ellipsis">{{ $item->type->label() }}</dd>
-                        @endif
+                    <div class="flex flex-col w-full">
+                        <h1 class="text-base font-semibold">{{ $item->name }}</h1>
+                        <dl class="dl text-sm text-secondary-300">
+                            @if ($item->type)
+                                <dt class="sr-only">{{ __('Type') }}</dt>
+                                <dd class="text-ellipsis">{{ $item->type->label() }}</dd>
+                            @endif
 
-                        <dt class="sr-only">{{ __('Videos') }}</dt>
-                        <dd class="text-ellipsis">{{ $item->videos_count }} videos</dd>
-                    </dl>
-                </div>
-            </article>
+                            <dt class="sr-only">{{ __('Videos') }}</dt>
+                            <dd class="text-ellipsis">{{ $item->videos_count }} videos</dd>
+                        </dl>
+                    </div>
+                </article>
+            </a>
         @empty
             {{ __('No items found') }}
         @endforelse
