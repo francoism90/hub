bdbce63c6a2073432d16852871fb9be7ecd5514f
francoism90 <francoism90@users.noreply.github.com>
2024-05-22 16:51:27 +0200

    Add flex-wrap

--- resources/views/livewire/dashboard/videos/tabs/general.blade.php
+++ resources/views/livewire/dashboard/videos/tabs/general.blade.php
@@ -13,7 +13,7 @@
             </x-slot:append>
         </x-dashboard.forms.input>
 
-        <x-app.layout.join>
+        <x-app.layout.join class="flex-wrap">
             <x-dashboard.forms.input
                 label="{{ __('Episode') }}"
                 placeholder="{{ __('E01') }}"
