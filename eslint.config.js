071f1b3654e1c17a05510b94a48d34370642b1d6
francoism90 <francoism90@users.noreply.github.com>
2024-05-22 10:49:42 +0200

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,7 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
-export default [{ languageOptions: { globals: { ...globals.browser, ...globals.node } } }, pluginJs.configs.recommended];
+export default [
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+];
