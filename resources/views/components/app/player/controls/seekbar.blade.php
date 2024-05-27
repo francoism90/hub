92e25de9e6ec34709332b13948d1594548b4f1dc
francoism90 <francoism90@users.noreply.github.com>
2024-05-27 21:27:44 +0200

    Use step any

--- resources/views/components/app/player/controls/seekbar.blade.php
+++ resources/views/components/app/player/controls/seekbar.blade.php
@@ -1,20 +1,23 @@
 @aware([
     'video',
 ])
+
 <div class="absolute inset-x-0 bottom-16 z-20">
     <div class="relative h-1.5 w-full bg-secondary-500/50">
         <progress
             x-model="bufferedPct(buffered, duration)"
             min="0"
             max="100"
-            class="progress progress-secondary absolute inset-0 z-0 h-full w-full"
+            step="any"
+            class="progress progress-secondary absolute inset-0 z-0"
         ></progress>
 
         <progress
             x-model="currentTime"
             :min="0"
             :max="duration"
-            class="progress progress-primary absolute inset-0 z-10 h-full w-full"
+            step="any"
+            class="progress progress-primary absolute inset-0 z-10"
         ></progress>
 
         <input
@@ -23,8 +26,8 @@
             x-on:input="seekTo"
             :min="0"
             :max="duration"
-            step="0.1"
-            class="range range-primary absolute inset-0 z-20 h-full w-full"
+            step="any"
+            class="range range-primary absolute inset-0 z-20"
         />
     </div>
 </div>
