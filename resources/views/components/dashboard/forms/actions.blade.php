50889c9543202d7e6d2180905230d9c7dd812374
francoism90 <francoism90@users.noreply.github.com>
2024-05-29 19:04:49 +0200

    Improve actions

--- resources/views/components/dashboard/forms/actions.blade.php
+++ resources/views/components/dashboard/forms/actions.blade.php
@@ -2,15 +2,11 @@
     'actions',
 ])
 
-<nav class="my-3 overflow-x-auto border border-y border-secondary-700/50 py-3">
-    <x-app.layout.container fluid>
-        <nav class="flex flex-nowrap items-center gap-x-3">
-            @foreach ($actions as $action)
-            <x-wireuse::actions-button :$action>
-                <x-wireuse::actions-icon :$action />
-                <span class="text-sm font-medium">{{ $action->getLabel() }}</span>
-            </x-wireuse::actions-button>
-            @endforeach
-        </nav>
-    </x-app.layout.container>
-</nav>
+<x-app.layout.container class="flex flex-nowrap items-center gap-x-3 overflow-x-auto" fluid>
+    @foreach ($actions as $action)
+        <x-wireuse::actions-button
+            :$action
+            class:label="text-sm font-medium"
+        />
+    @endforeach
+</x-app.layout.container>
