69dd62c580ad6b9cee7a7439a9647635b44c7fd9
francoism90 <francoism90@users.noreply.github.com>
2024-05-30 20:00:11 +0200

    wip

--- src/App/Livewire/Feed/Video.php
+++ src/App/Livewire/Feed/Video.php
@@ -27,7 +27,7 @@ class Video extends Component
                 ->icon('heroicon-o-arrow-path-rounded-square')
                 ->iconActive('heroicon-s-arrow-path-rounded-square')
                 ->componentAttributes([
-                    'wire:click.prevent' => '$parent.clear()',
+                    'wire:click' => '$parent.clear()',
                 ]),
 
             Action::make('previews')
