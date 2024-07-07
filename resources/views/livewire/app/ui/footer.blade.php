f1f2120185db7dd0becf830c87c02a43384987df
francoism90 <francoism90@users.noreply.github.com>
2024-07-07 23:41:40 +0200

    wip

--- resources/views/livewire/app/ui/footer.blade.php
+++ resources/views/livewire/app/ui/footer.blade.php
@@ -1,6 +1,6 @@
 {{ html()->element('footer')->class('sticky bottom-0 z-30 h-16 max-h-16 w-full bg-secondary-950 border-t border-secondary-800/80')->children([
     html()->div()->class('container flex h-16 justify-between gap-x-3 overflow-x-auto sm:gap-x-12')->children([
-        html()->icon('heroicon-o-square-2-stack'),
+        html()->icon('heroicon-o-square-2-stack', 'text-white size-5')->class('foo'),
         // html()->a()->class('link')->route('home')->text('Home')->prependChild(
         //     html()->icon('heroicon-o-square-2-stack'),
         // ),
