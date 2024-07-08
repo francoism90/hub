2ec609b5e900307c32d7e732c626dee2549fa29d
francoism90 <francoism90@users.noreply.github.com>
2024-07-08 21:22:32 +0200

    wip

--- resources/views/components/app/ui/footer.blade.php
+++ resources/views/components/app/ui/footer.blade.php
@@ -2,7 +2,7 @@
     $isActive = fn (string $route) => request()->routeIs($route, "{$route}.*");
 @endphp
 
-{{ html()->element('footer')->class('sticky bottom-0 z-30 h-16 max-h-16 w-full bg-secondary-950 border-t border-secondary-800/80')->children([
+{{ html()->element('footer')->class('sticky bottom-0 z-30 h-16 min-h-16 w-full bg-secondary-950 border-t border-secondary-800/80')->children([
     html()->element('nav')->class('*:flex-col *:gap-1 *:py-0.5 *:text-sm *:font-medium *:line-clamp container h-full max-w-xl flex flex-nowrap items-stretch justify-between gap-x-3 overflow-x-auto sm:gap-x-12')->children([
         html()->a()->route('home')->text('Home')->prependChild(
             html()->icon()->svg($isActive('home') ? 'heroicon-s-home' : 'heroicon-s-home', 'size-6'),
