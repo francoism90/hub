5b3056d0836fb1fb6842b17162f7e015804f3b2b
francoism90 <francoism90@users.noreply.github.com>
2024-07-09 19:56:16 +0200

    wip

--- resources/views/app/search/index.blade.php
+++ resources/views/app/search/index.blade.php
@@ -2,7 +2,7 @@
     {{ html()->element('h1')->text('Search')->class('text-2xl') }}
 
     {{ html()->div()->class('flex pb-3 items-center gap-2.5')->children([
-        html()->input('search')->wireModel('form.query', 'live.debounce')->class('input input-bordered w-full')->placeholder('Search'),
+        html()->input('search', 'query')->wireModel('form.query', 'live.debounce')->class('input input-bordered w-full')->placeholder('Search'),
     ]) }}
 
     {{ html()->element('section')->class('grid grow grid-cols-1 gap-3.5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4')->open() }}
