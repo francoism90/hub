eebf9898ebae16ab9295c53f7bd063c0f3dd6266
francoism90 <francoism90@users.noreply.github.com>
2024-07-09 22:34:03 +0200

    wip

--- resources/views/app/search/index.blade.php
+++ resources/views/app/search/index.blade.php
@@ -1,9 +1,9 @@
 {{ html()->div()->attribute('x-data', 'player')->class('container py-4')->open() }}
     {{ html()->element('h1')->text('Search')->class('text-2xl') }}
 
-    {{ html()->wireForm($form, 'submit')->class('block pt-2')->child(
+    {{ html()->wireForm($form, 'submit')->class('block pt-2')->children([
         html()->input('search', 'query')->wireModel('form.query', 'live.debounce')->class('input input-bordered w-full')->placeholder('Search'),
-    ) }}
+    ]) }}
 
     @if ($this->items->isNotEmpty())
         {{ html()->element('section')->wireKey($this->hash)->class('pt-4 grid grow grid-cols-1 gap-3.5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4')->open() }}
