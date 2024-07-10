c02e248576411f73fb1cd6edae5ff3c72f217823
francoism90 <francoism90@users.noreply.github.com>
2024-07-10 21:29:17 +0200

    wip

--- resources/views/components/app/ui/header.blade.php
+++ resources/views/components/app/ui/header.blade.php
@@ -1,4 +1,4 @@
-{{ html()->element('header')->class('h-14 min-h-14 bg-secondary-950 border-b border-secondary-800/80')->children([
+{{ html()->element('header')->class('h-14 min-h-14 w-full bg-secondary-950 border-b border-secondary-800/80')->children([
     html()->element('nav')->class('container mx-0 max-w-none h-full flex flex-nowrap items-stretch justify-between gap-x-3 overflow-x-auto sm:gap-x-12')->children([
         html()->div()->class('inline-flex w-2/4 items-center justify-start')->child(
             html()->a()->route('home')->children([
--- src/App/Web/Library/Forms/QueryForm.php
+++ src/App/Web/Library/Forms/QueryForm.php
@@ -11,6 +11,17 @@ class QueryForm extends Form
 
     protected static bool $recoverable = true;
 
+    #[Validate('nullable|string|max:255')]
+    public string $query = '';
+
     #[Validate('nullable|string|in:recent,updated')]
     public string $sort = '';
+
+    public function query(): string
+    {
+        return str($this->get('query', ''))
+            ->title()
+            ->squish()
+            ->value();
+    }
 }
