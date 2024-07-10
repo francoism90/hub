fd29add2a320e9073ab869a4a8ad98ccb0f93b30
francoism90 <francoism90@users.noreply.github.com>
2024-07-10 13:08:06 +0200

    wip

--- src/App/Web/Search/Forms/QueryForm.php
+++ src/App/Web/Search/Forms/QueryForm.php
@@ -10,8 +10,6 @@ class QueryForm extends Form
 {
     protected static bool $store = true;
 
-    protected static bool $recoverable = true;
-
     #[Validate('nullable|string|max:255')]
     public string $query = '';
 
