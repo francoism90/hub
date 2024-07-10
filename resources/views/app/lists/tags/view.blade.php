0f578686a5b29cc896afaa29073abedcf24b490c
francoism90 <francoism90@users.noreply.github.com>
2024-07-10 16:20:01 +0200

    wip

--- resources/views/app/lists/tags/view.blade.php
+++ resources/views/app/lists/tags/view.blade.php
@@ -1,7 +1,6 @@
 {{ html()->div()->class('flex flex-col w-full gap-y-3')->open() }}
     {{ html()->element('h1')->text($title)->class('text-2xl') }}
 
-    @persist('scrollbar')
     {{ html()->div()->class('flex flex-col gap-3 w-full overflow-y-scroll')->attribute('wire:scroll')->open() }}
         @foreach ($this->items as $tag)
             <livewire:app::tags-item :$tag :key="$tag->getRouteKey()" />
@@ -9,6 +8,5 @@
 
         {{ html()->button()->text('Load More')->class('btn btn-primary')->attribute('wire:click', 'fetch') }}
     {{ html()->div()->close() }}
-    @endpersist
 {{ html()->div()->close() }}
 
