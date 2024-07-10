80730de109683ca314c14207e711a42b5e046028
francoism90 <francoism90@users.noreply.github.com>
2024-07-10 21:42:34 +0200

    wip

--- src/App/Web/Videos/Components/Watching.php
+++ src/App/Web/Videos/Components/Watching.php
@@ -2,6 +2,7 @@
 
 namespace App\Web\Videos\Components;
 
+use Domain\Playlists\Models\Playlist;
 use Illuminate\Support\Collection;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Lazy;
@@ -12,8 +13,13 @@ class Watching extends Section
     #[Computed]
     public function items(): Collection
     {
-        return $this->getQuery()
+        $playlist = Playlist::findByName(auth()->user(), 'history');
+
+        $this->authorize('view', $playlist);
+
+        return $playlist->videos()
             ->published()
+            ->orderByDesc('videoables.created_at')
             ->take(16)
             ->get();
     }
