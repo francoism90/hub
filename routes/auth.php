1fd97a296fe0a6f22e38eda7ed3af81433308028
francoism90 <francoism90@users.noreply.github.com>
2024-07-11 11:16:41 +0200

    wip

--- routes/auth.php
+++ routes/auth.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Web\Auth\Controllers\LoginController;
+use App\Web\Auth\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', LoginController::class)->middleware('guest')->name('login');
-// Route::get('/register', RegisterController::class)->middleware('guest')->name('register');
-// Route::post('/logout', LogoutController::class)->name('logout');
+Route::get('/logout', LogoutController::class)->name('logout');
