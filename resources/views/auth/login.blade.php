e8ed287c769eee1b8867492df334a0345192f82a
francoism90 <francoism90@users.noreply.github.com>
2024-07-11 11:12:54 +0200

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     html()->div()->class('container w-[28rem] max-w-[28rem] bg-secondary-500/50 rounded px-8 py-5')->children([
         html()->element('h1')->text('Log In')->class('text-3xl hyphens-auto line-clamp-2'),
 
-        html()->wireForm($form, 'submit')->class('flex flex-col py-6 gap-y-6')->children([
+        html()->wireForm($form, 'submit')->class('flex flex-col py-3 gap-y-6')->children([
             html()->div()->classIf(flash()->message, 'form-message')->textIf(flash()->message, flash()->message),
 
             html()->div()->class('form-control')->children([
@@ -18,9 +18,9 @@
             ]),
 
             html()->div()->class('form-control-inline')->children([
-                html()->checkbox()->wireModel('form.password')->placeholder('Your Password')->class('checkbox checkbox-primary'),
-                html()->label('Password', 'form.password')->class('label'),
-                html()->validate('form.password'),
+                html()->checkbox()->wireModel('form.remember')->class('checkbox checkbox-primary'),
+                html()->label('Remember me', 'form.remember')->class('label'),
+                html()->validate('form.remember'),
             ]),
 
             html()->button()->text('Log In')->class('btn btn-secondary')
