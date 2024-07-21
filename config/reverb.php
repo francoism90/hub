6cd6626cd39209a65b7c3e6185b1250d8ae4f848
francoism90 <francoism90@users.noreply.github.com>
2024-07-21 02:30:09 +0200

    Update Reverb configuration to use secure WebSocket connection

--- config/reverb.php
+++ config/reverb.php
@@ -30,7 +30,7 @@ return [
 
         'reverb' => [
             'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
-            'port' => env('REVERB_SERVER_PORT', 8080),
+            'port' => env('REVERB_SERVER_PORT', 6001),
             'hostname' => env('REVERB_HOST'),
             'options' => [
                 'tls' => [],
