66f2976db6867de8cd50045ff4955cf642dcb7ca
francoism90 <francoism90@users.noreply.github.com>
2024-08-04 16:09:59 +0200

    chore: Update LaravelExtraIntellisense settings to include modelsPaths

--- resources/js/vendor/echo.js
+++ resources/js/vendor/echo.js
@@ -1,15 +1,15 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-  broadcaster: 'reverb',
+  broadcaster: "reverb",
   key: import.meta.env.VITE_REVERB_APP_KEY,
   wsHost: import.meta.env.VITE_REVERB_HOST,
   wsPort: import.meta.env.VITE_REVERB_PORT,
   wssPort: import.meta.env.VITE_REVERB_PORT,
-  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-  authEndpoint: '/api/v1/broadcasting/auth',
-  enabledTransports: ['ws', 'wss'],
+  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+  authEndpoint: "/api/v1/broadcasting/auth",
+  enabledTransports: ["ws", "wss"],
 });
