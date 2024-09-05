14e86aa55f1f4c67f9aa8a3198e62359ef616c25
francoism90 <francoism90@users.noreply.github.com>
2024-09-05 14:16:52 +0200

    feat: Add create user command

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -57,6 +57,7 @@ $app = Application::configure(basePath: $basePath)
         \Support\Structures\Commands\RefreshStructures::class,
         \Domain\Tags\Commands\CreateTag::class,
         \Domain\Tags\Commands\SortTags::class,
+        \Domain\Users\Commands\CreateUser::class,
         \Domain\Videos\Commands\CleanVideos::class,
         \Domain\Videos\Commands\ImportVideos::class,
     ])
