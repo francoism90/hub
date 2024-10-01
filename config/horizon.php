10f73a771fa5326200b5da214181b7badd6cbf89
francoism90 <francoism90@users.noreply.github.com>
2024-10-01 19:55:30 +0200

    Update time values for trimming in horizon.php

--- config/horizon.php
+++ config/horizon.php
@@ -102,9 +102,9 @@ return [
     */
 
     'trim' => [
-        'recent' => 10080,
-        'pending' => 10080,
-        'completed' => 10080,
+        'recent' => 1440,
+        'pending' => 1440,
+        'completed' => 1440,
         'recent_failed' => 10080,
         'failed' => 10080,
         'monitored' => 10080,
