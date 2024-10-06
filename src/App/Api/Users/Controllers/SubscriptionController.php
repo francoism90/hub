4323fcdfccf225a6ac8ec7dc036afd2862b1bcce
francoism90 <francoism90@users.noreply.github.com>
2024-10-06 22:58:09 +0200

    Check auth

--- src/App/Api/Users/Controllers/SubscriptionController.php
+++ src/App/Api/Users/Controllers/SubscriptionController.php
@@ -23,6 +23,7 @@ class SubscriptionController extends Controller implements HasMiddleware
 
     public function __invoke(Request $request): UserResource
     {
+        logger($request->headers->all());
         Gate::authorize('update', $request->user());
 
         return new UserResource($request->user());
