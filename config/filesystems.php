eb5ea0f840c32653423ba4c268c56ed7df20dc72
francoism90 <francoism90@users.noreply.github.com>
2024-10-06 02:29:17 +0200

    wip

--- config/filesystems.php
+++ config/filesystems.php
@@ -84,7 +84,7 @@ return [
             'url' => env('AWS_URL') . '/conversion',
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'visibility' => 'private',
+            'visibility' => 'public',
             'throw' => true,
             'read-only' => false,
             'options' => [
