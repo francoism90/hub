65c5e51fa927d6a14a65e97e0cb7eb3c6a7c8d56
francoism90 <francoism90@users.noreply.github.com>
2024-10-10 16:54:01 +0200

    Refactor button and pagination styles

--- resources/views/vendor/livewire/simple-tailwind.blade.php
+++ resources/views/vendor/livewire/simple-tailwind.blade.php
@@ -12,20 +12,28 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
 
 <div>
     @if ($paginator->hasPages())
-        <nav role="navigation" aria-label="Pagination Navigation" class="flex items-center gap-1.5">
+        <nav role="navigation" aria-label="Pagination Navigation" class="flex items-center justify-center gap-2">
             @if ($paginator->onFirstPage())
                 <button
                     type="button"
                     disabled
                     aria-label="{{ __('pagination.previous') }}"
-                    class="relative inline-flex items-center p-1.5 border border-secondary-400/50 text-secondary-500 cursor-default select-none"
+                    class="btn btn-secondary btn-outlined btn-square"
                 >
                     <x-heroicon-o-chevron-double-left class="size-4" />
                 </button>
             @else
                 @if(method_exists($paginator,'getCursorName'))
-                    <button type="button" dusk="previousPage" wire:key="cursor-{{ $paginator->getCursorName() }}-{{ $paginator->previousCursor()->encode() }}" wire:click="setPage('{{$paginator->previousCursor()->encode()}}','{{ $paginator->getCursorName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                            {!! __('pagination.previous') !!}
+                    <button
+                        type="button"
+                        dusk="previousPage"
+                        wire:key="cursor-{{ $paginator->getCursorName() }}-{{ $paginator->previousCursor()->encode() }}"
+                        wire:click="setPage('{{$paginator->previousCursor()->encode()}}','{{ $paginator->getCursorName() }}')"
+                        x-on:click="{{ $scrollIntoViewJsSnippet }}"
+                        wire:loading.attr="disabled"
+                        class="btn btn-secondary btn-outlined btn-square"
+                    >
+                        {!! __('pagination.previous') !!}
                     </button>
                 @else
                     <button
@@ -35,7 +43,7 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                         wire:loading.attr="disabled"
                         dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}"
                         aria-label="{{ __('pagination.previous') }}"
-                        class="relative inline-flex items-center p-1.5 border border-secondary-400/50 text-secondary-500 hover:text-secondary-400 focus:outline-none focus:border-secondary-300 transition ease-in-out duration-150"
+                        class="btn btn-secondary btn-outlined btn-square"
                     >
                         <x-heroicon-o-chevron-double-left class="size-4" />
                     </button>
@@ -55,7 +63,7 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                         wire:loading.attr="disabled"
                         dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}"
                         aria-label="{{ __('pagination.next') }}"
-                        class="relative inline-flex items-center p-1.5 border border-secondary-400/50 text-secondary-500 hover:text-secondary-400 focus:outline-none focus:border-secondary-300 transition ease-in-out duration-150"
+                        class="btn btn-secondary btn-outlined btn-square"
                     >
                         <x-heroicon-o-chevron-double-right class="size-4" />
                     </button>
@@ -65,7 +73,7 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                     type="button"
                     disabled
                     aria-label="{{ __('pagination.next') }}"
-                    class="relative inline-flex items-center p-1.5 border border-secondary-400/50 text-secondary-500 cursor-default select-none"
+                    class="btn btn-secondary btn-outlined btn-square"
                 >
                     <x-heroicon-o-chevron-double-right class="size-4" />
                 </button>
