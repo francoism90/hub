6d7d45601a10d1231a14a942d15cf9b7750a48db
francoism90 <francoism90@users.noreply.github.com>
2024-10-12 22:59:35 +0200

    Refactor create_videos_table migration to remove nullableMorphs column

--- database/migrations/2022_12_29_100035_create_videos_table.php
+++ database/migrations/2022_12_29_100035_create_videos_table.php
@@ -10,7 +10,6 @@ return new class extends Migration
     {
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
-            $table->nullableMorphs('model');
             $table
                 ->foreignId('user_id')
                 ->constrained()
