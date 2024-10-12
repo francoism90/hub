c2c3cce840d3edb297f2801e17e17ea1d1c503e6
francoism90 <francoism90@users.noreply.github.com>
2024-10-12 23:04:36 +0200

    Refactor create_userables_table migration to cascade delete on user_id foreign key

--- database/migrations/2023_08_28_154623_create_userables_table.php
+++ database/migrations/2023_08_28_154623_create_userables_table.php
@@ -9,7 +9,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('userables', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
             $table->morphs('userable');
             $table->json('options')->nullable();
             $table->timestamps();
