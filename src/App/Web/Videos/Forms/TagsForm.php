41fdb3dcbeed201914fa0a594b8ea1d3cc256ae4
francoism90 <francoism90@users.noreply.github.com>
2024-10-12 18:28:46 +0200

    Refactor filesystem configuration to use private storage path for local driver

--- src/App/Web/Videos/Forms/TagsForm.php
+++ src/App/Web/Videos/Forms/TagsForm.php
@@ -5,6 +5,7 @@ namespace App\Web\Videos\Forms;
 use Domain\Tags\Models\Tag;
 use Foxws\WireUse\Forms\Support\Form;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Validate;
 
@@ -37,10 +38,13 @@ class TagsForm extends Form
     #[Computed(persist: true)]
     public function popular(): Collection
     {
-        return Tag::query()
-            ->withCount('videos')
-            ->orderByDesc('videos_count')
+        return DB::table('taggables')
+            ->selectRaw('id, count(tag_id) as tagged_count')
+            ->join('tags', 'tags.id', '=', 'taggables.tag_id')
+            ->groupBy('tags.id')
+            ->orderBy('tagged_count', 'desc')
             ->take(16)
-            ->get();
+            ->get()
+            ->map(fn (\stdClass $tag) => Tag::find($tag->id));
     }
 }
