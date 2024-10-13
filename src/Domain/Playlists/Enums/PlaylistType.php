a014b1c9c401c063a7c1191b7bd968b1df10baeb
francoism90 <francoism90@users.noreply.github.com>
2024-10-13 10:40:00 +0200

    Refactor PlaylistType enum to include Private and Public options

--- src/Domain/Playlists/Enums/PlaylistType.php
+++ src/Domain/Playlists/Enums/PlaylistType.php
@@ -6,12 +6,16 @@ enum PlaylistType: string
 {
     case System = 'system';
     case Mixer = 'mixer';
+    case Private = 'private';
+    case Public = 'public';
 
     public function label(): string
     {
         return match ($this) {
             self::System => __('System'),
             self::Mixer => __('Mixer'),
+            self::Private => __('Private'),
+            self::Public => __('Public'),
         };
     }
 }
