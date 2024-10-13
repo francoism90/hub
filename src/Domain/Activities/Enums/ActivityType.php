2cb698ced710f4fc0a05797a4a83babe66cba39e
francoism90 <francoism90@users.noreply.github.com>
2024-10-13 17:37:40 +0200

    Refactor ActivityType enum to include Reaction type

--- src/Domain/Activities/Enums/ActivityType.php
+++ src/Domain/Activities/Enums/ActivityType.php
@@ -7,6 +7,7 @@ enum ActivityType: string
     case Bookmark = 'bookmark';
     case Favorite = 'favorite';
     case Like = 'like';
+    case Reaction = 'reaction';
 
     public function label(): string
     {
@@ -14,6 +15,7 @@ enum ActivityType: string
             self::Bookmark => __('Bookmark'),
             self::Favorite => __('Favorite'),
             self::Like => __('Like'),
+            self::Reaction => __('Reaction'),
         };
     }
 }
