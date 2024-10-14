d8a9346c0ab2a6b7c54c2e2ebb907274f102a74d
francoism90 <francoism90@users.noreply.github.com>
2024-10-14 14:41:11 +0200

    wip

--- database/migrations/2022_12_29_100035_create_videos_table.php
+++ database/migrations/2022_12_29_100035_create_videos_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
     {
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
+            $table->nullableMorphs('model');
             $table
                 ->foreignId('user_id')
                 ->constrained()
