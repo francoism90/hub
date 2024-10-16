01a78ee21ff283d5f9a21bebe50c0d5f1464ecde
francoism90 <francoism90@users.noreply.github.com>
2024-10-16 16:25:47 +0200

    wip

---
+++ src/Domain/Groups/Actions/CreateSystemGroups.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Groups\Actions;
+
+use Domain\Groups\Enums\GroupSet;
+use Domain\Groups\Enums\GroupType;
+use Domain\Users\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class CreateSystemGroups
+{
+    public function execute(User $user): void
+    {
+        DB::transaction(function () use ($user) {
+            collect($this->getCollection())->each(
+                fn (GroupSet $group) => app(CreateNewGroup::class)->execute($user, [
+                    'kind' => $group,
+                    'type' => GroupType::System,
+                ])
+            );
+        });
+    }
+
+    protected function getCollection(): array
+    {
+        return [
+            GroupSet::Favorite,
+            GroupSet::Saved,
+            GroupSet::Viewed,
+        ];
+    }
+}
