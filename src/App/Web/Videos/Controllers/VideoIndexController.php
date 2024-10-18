166d1796c988c5201b9600f0e7e4662cc963738e
francoism90 <francoism90@users.noreply.github.com>
2024-10-18 14:40:48 +0200

    wip

--- src/App/Web/Videos/Controllers/VideoIndexController.php
+++ src/App/Web/Videos/Controllers/VideoIndexController.php
@@ -4,17 +4,31 @@ declare(strict_types=1);
 
 namespace App\Web\Videos\Controllers;
 
-use App\Web\Videos\Forms\QueryForm;
 use Domain\Groups\Actions\CreateMixerGroups;
 use Domain\Groups\Enums\GroupSet;
 use Domain\Tags\Models\Tag;
 use Foxws\WireUse\Views\Support\Page;
 use Illuminate\Support\Collection;
-use Illuminate\View\View;
 use Livewire\Attributes\Computed;
+use App\Web\Videos\Forms\QueryForm;
+use App\Web\Videos\Scopes\FilterVideos;
+use Domain\Videos\Models\Video;
+use Foxws\WireUse\Auth\Concerns\WithAuthentication;
+use Foxws\WireUse\Layout\Concerns\WithScroll;
+use Foxws\WireUse\Models\Concerns\WithQueryBuilder;
+use Illuminate\Pagination\Paginator;
+use Illuminate\View\View;
+use Livewire\Attributes\Modelable;
+use Livewire\Component;
+use Livewire\WithoutUrlPagination;
 
 class VideoIndexController extends Page
 {
+    use WithAuthentication;
+    use WithoutUrlPagination;
+    use WithQueryBuilder;
+    use WithScroll;
+
     public QueryForm $form;
 
     public function mount(): void
@@ -32,6 +46,19 @@ class VideoIndexController extends Page
     public function updatedForm(): void
     {
         $this->form->submit();
+
+        $this->clear();
+    }
+
+    public function populate(): void
+    {
+        $this->setupMixers(force: true);
+
+        unset($this->lists);
+
+        $this->form->reset('list');
+
+        $this->refresh();
     }
 
     public function refresh(): void
@@ -43,15 +70,15 @@ class VideoIndexController extends Page
 
     public function reload(): void
     {
-        $this->setupMixers(force: true);
+        $this->clear();
 
-        $this->form->reset('list');
+        $this->fetch();
 
         $this->refresh();
     }
 
     #[Computed(persist: true, seconds: 3600)]
-    public function items(): Collection
+    public function lists(): Collection
     {
         $items = collect($this->getAuthModel()->storeValue('mixers'));
 
@@ -72,6 +99,20 @@ class VideoIndexController extends Page
         return $items;
     }
 
+    protected function getPageItems(?int $page = null): Paginator
+    {
+        $page ??= $this->getPage() ?? 1;
+
+        return $this->getQuery()->tap(
+            new FilterVideos(form: $this->form, user: $this->getAuthModel())
+        )->simplePaginate(perPage: 16, page: $page);
+    }
+
+    protected function getModelClass(): ?string
+    {
+        return Video::class;
+    }
+
     protected function setupMixers(?bool $force = null): void
     {
         app(CreateMixerGroups::class)->execute($this->getAuthModel(), $force);
@@ -85,5 +126,15 @@ class VideoIndexController extends Page
     protected function getDescription(): ?string
     {
         return __('Browse and watch videos');
+    }
+
+    public function getListeners(): array
+    {
+        $id = $this->getAuthKey();
+
+        return [
+            "echo-private:user.{$id},.video.trashed" => 'refresh',
+            "echo-private:user.{$id},.video.updated" => 'refresh',
+        ];
     }
 }
