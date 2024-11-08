8ffea3e6259cdabcdd6e54de94ca9650a7faffc1
francoism90 <francoism90@users.noreply.github.com>
2024-11-08 18:33:55 +0100

    Fix create tag

--- src/Domain/Tags/Actions/CreateNewTag.php
+++ src/Domain/Tags/Actions/CreateNewTag.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Domain\Tags\Actions;
 
+use Domain\Tags\Enums\TagType;
 use Domain\Tags\Models\Tag;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
@@ -13,9 +14,11 @@ class CreateNewTag
     public function execute(array $attributes): Tag
     {
         return DB::transaction(function () use ($attributes) {
-            $model = Tag::firstOrCreate(
-                Arr::only($attributes, ['name', 'type']),
-                Arr::only($attributes, app(Tag::class)->getFillable()),
+            $attributes['type'] = TagType::from($attributes['type'])->value;
+
+            $model = Tag::findOrCreate(
+                $attributes['name'],
+                $attributes['type'],
             );
 
             return $model;
