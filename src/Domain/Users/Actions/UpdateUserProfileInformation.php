f20bc11315577826b204da871b04dbfbc9e9c638
francoism90 <francoism90@users.noreply.github.com>
2024-11-19 09:29:13 +0100

    wip

--- src/Domain/Users/Actions/UpdateUserProfileInformation.php
+++ src/Domain/Users/Actions/UpdateUserProfileInformation.php
@@ -7,18 +7,21 @@ namespace Domain\Users\Actions;
 use Domain\Users\Models\User;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 
 class UpdateUserProfileInformation
 {
-    public function execute(User $user, array $attributes): mixed
+    public function execute(User $user, array $attributes): void
     {
-        if (array_key_exists('email', $attributes) && ($user->email !== $attributes['email'] && $user instanceof MustVerifyEmail)) {
-            return $this->updateVerifiedUser($user, $attributes);
-        }
+        DB::transaction(function () use ($user, $attributes) {
+            if (array_key_exists('email', $attributes) && ($user->email !== $attributes['email'] && $user instanceof MustVerifyEmail)) {
+                return $this->updateVerifiedUser($user, $attributes);
+            }
 
-        $user->updateOrFail(
-            Arr::only($attributes, $user->getFillable())
-        );
+            $user->updateOrFail(
+                Arr::only($attributes, $user->getFillable())
+            );
+        });
     }
 
     protected function updateVerifiedUser(User $user, array $attributes): void
