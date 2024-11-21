954df82f3ea9e144f893bb5daa78a1426cdfd6b8
francoism90 <francoism90@users.noreply.github.com>
2024-11-21 11:48:40 +0100

    Update wire polling interval from 900s to 600s across multiple views

--- resources/views/app/search/index.blade.php
+++ resources/views/app/search/index.blade.php
@@ -44,7 +44,7 @@
 
         {{ html()
             ->element('main')
-            ->attribute('wire:poll.900s')
+            ->attribute('wire:poll.600s')
             ->class('grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4')
             ->open()
         }}
--- resources/views/app/tags/section/view.blade.php
+++ resources/views/app/tags/section/view.blade.php
@@ -1,7 +1,7 @@
 {{ html()->div()->class('flex flex-col w-full gap-y-2')->open() }}
     {{ html()->element('h1')->text($title)->class('text-lg') }}
 
-    {{ html()->div()->attributes(['wire:scroll', 'wire:poll.900s'])->class('grid grid-cols-1 gap-3 w-full overflow-y-scroll sm:grid-cols-3')->open() }}
+    {{ html()->div()->attributes(['wire:scroll', 'wire:poll.600s'])->class('grid grid-cols-1 gap-3 w-full overflow-y-scroll sm:grid-cols-3')->open() }}
         @foreach ($this->items as $tag)
             {{ html()->div()->wireKey($tag->getRouteKey())->open() }}
                 <livewire:web.tags.item :$tag :key="$tag->getRouteKey()" />
