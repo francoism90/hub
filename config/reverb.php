6cf1cbbd1af4db755dd3319137d5abf32ef9e930
francoism90 <francoism90@users.noreply.github.com>
2024-11-26 18:53:51 +0100

    feat: update Reverb configuration to remove TLS local certificate and adjust NGINX server name

--- config/reverb.php
+++ config/reverb.php
@@ -33,9 +33,7 @@ return [
             'port' => env('REVERB_SERVER_PORT', 6001),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [
-                    'local_cert' => '/run/secrets/cert.pem',
-                ],
+                'tls' => [],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
