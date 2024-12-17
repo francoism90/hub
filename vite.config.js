3ece7157607ac73d9c4159b581ae590e26bf1f68
francoism90 <francoism90@users.noreply.github.com>
2024-12-17 14:39:59 +0100

    refactor: update .env.example with empty AWS credentials and change log channel to stack; add basic SSL plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,100 +1,86 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
-import { readFileSync } from "fs";
-import { fileURLToPath, URL } from "url";
 import { VitePWA } from "vite-plugin-pwa";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import laravel, { refreshPaths } from "laravel-vite-plugin";
 
-export default defineConfig(({ mode }) => {
-  let https = false;
-
-  if (mode === "development") {
-    https = {
-      cert: readFileSync("/run/secrets/cert.pem"),
-      key: readFileSync("/run/secrets/key.pem"),
-    };
-  }
-
-  return {
-    server: {
-      host: "0.0.0.0",
-      https,
-      port: 5173,
-      strictPort: true,
-      hmr: {
-        host: "hub.lan",
-        clientPort: 5173,
-      },
-      watch: {
-        ignored: ["**/storage/**"],
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    hmr: { host: "localhost" },
+    https: {},
+    watch: {
+      ignored: ["**/storage/**"],
     },
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./", import.meta.url)),
-        "~": fileURLToPath(new URL("./node_modules", import.meta.url)),
-        "!": fileURLToPath(new URL("./vendor", import.meta.url)),
-      },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./", import.meta.url)),
+      "~": fileURLToPath(new URL("./node_modules", import.meta.url)),
+      "!": fileURLToPath(new URL("./vendor", import.meta.url)),
     },
-    plugins: [
-      laravel({
-        input: ["resources/css/app.css", "resources/js/app.js"],
-        refresh: [...refreshPaths, "resources/**", "src/**"],
-      }),
-      VitePWA({
-        buildBase: "/build/",
+  },
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      refresh: [...refreshPaths, "resources/**", "src/**"],
+    }),
+    VitePWA({
+      buildBase: "/build/",
+      scope: "/",
+      registerType: "autoUpdate",
+      injectRegister: "script-defer",
+      workbox: {
+        cleanupOutdatedCaches: true,
+        directoryIndex: null,
+        globDirectory: "/app/public/build",
+        globPatterns: ["**/*.{js,css,html,svg,jpg,png,webp,ico,txt,woff,woff2}"],
+        maximumFileSizeToCacheInBytes: 4194304,
+        navigateFallback: null,
+        navigateFallbackDenylist: [/\/[api,livewire,live,vod]+\/.*/],
+      },
+      manifest: {
+        name: "Hub",
+        short_name: "Hub",
+        description: "A video on demand (VOD) media distribution system.",
+        categories: ["videos", "streaming", "vod"],
+        theme_color: "#030712",
+        background_color: "#030712",
+        display_override: ["standalone", "minimal-ui"],
+        display: "standalone",
+        orientation: "natural",
+        id: "/",
         scope: "/",
-        registerType: "autoUpdate",
-        injectRegister: "script-defer",
-        workbox: {
-          cleanupOutdatedCaches: true,
-          directoryIndex: null,
-          globDirectory: "/app/public/build",
-          globPatterns: ["**/*.{js,css,html,svg,jpg,png,webp,ico,txt,woff,woff2}"],
-          maximumFileSizeToCacheInBytes: 4194304,
-          navigateFallback: null,
-          navigateFallbackDenylist: [/\/[api,livewire,live,vod]+\/.*/],
-        },
-        manifest: {
-          name: "Hub",
-          short_name: "Hub",
-          description: "A video on demand (VOD) media distribution system.",
-          categories: ["videos", "streaming", "vod"],
-          theme_color: "#030712",
-          background_color: "#030712",
-          display_override: ["standalone", "minimal-ui"],
-          display: "standalone",
-          orientation: "natural",
-          id: "/",
-          scope: "/",
-          start_url: "/",
-          icons: [
-            {
-              src: "/storage/images/android-chrome-192x192.png",
-              sizes: "192x192",
-              type: "image/png",
-            },
-            {
-              src: "/storage/images/android-chrome-512x512.png",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: "any maskable",
-            },
-          ],
-        },
-      }),
-    ],
-    build: {
-      chunkSizeWarningLimit: 1024,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            utils: ["axios"],
-            ws: ["pusher-js", "laravel-echo"],
-            player: ["shaka-player"],
-            pwa: ["virtual:pwa-register"],
+        start_url: "/",
+        icons: [
+          {
+            src: "/storage/images/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
+          {
+            src: "/storage/images/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1024,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          utils: ["axios"],
+          ws: ["pusher-js", "laravel-echo"],
+          player: ["shaka-player"],
+          pwa: ["virtual:pwa-register"],
         },
       },
     },
-  };
+  },
 });
