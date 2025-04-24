eabaa01da30d74a1750358c40a66b0c29dcca007
francoism90 <francoism90@users.noreply.github.com>
2025-04-24 17:10:33 +0200

    Refactor EnsureRequestHasPrivateSubnet middleware: simplify handle method by removing unnecessary redirect logic

--- src/Foundation/Http/Middlewares/EnsureRequestHasPrivateSubnet.php
+++ src/Foundation/Http/Middlewares/EnsureRequestHasPrivateSubnet.php
@@ -7,22 +7,14 @@ namespace Foundation\Http\Middlewares;
 use Closure;
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\URL;
 use Symfony\Component\HttpFoundation\IpUtils;
 
 class EnsureRequestHasPrivateSubnet extends Middleware
 {
-    public function handle(Request $request, Closure $next, ?string $redirectToRoute = null): mixed
+    public function handle(Request $request, Closure $next): mixed
     {
-        $isPrivateIp = IpUtils::isPrivateIp($request->ip());
+        abort_unless(IpUtils::isPrivateIp($request->ip()), 403, 'Unauthorized');
 
-        if ($isPrivateIp) {
-            return $next($request);
-        }
-
-        return $request->expectsJson()
-            ? abort(403, 'Unauthorized')
-            : Redirect::guest(URL::route($redirectToRoute ?: 'home'));
+        return $next($request);
     }
 }
