39e18fad906921086c0f60fefb7c418a8b28b6f8
francoism90 <francoism90@users.noreply.github.com>
2025-05-22 17:25:31 +0200

    fix: refine video element attributes and improve event handling for better performance

--- resources/views/app/videos/card/view.blade.php
+++ resources/views/app/videos/card/view.blade.php
@@ -3,75 +3,75 @@
 {{ html()
     ->element('article')
     ->data('manifest', $video->preview)
+    ->class('card w-full h-60 min-w-60 min-h-60 max-h-60')
     ->attributes([
         'x-data' => '{ shown: false, manifest: $el.dataset.manifest }',
         'x-intersect.once' => 'shown = true',
     ])
-    ->class('card w-full h-60 min-w-60 min-h-60 max-h-60')
     ->children([
-        html()->a()->link('videos.view', $video)->class('relative shrink-0 block w-full h-48 min-h-48 max-h-48')->children([
-            html()
-                ->div()
-                ->class('absolute inset-0 z-20 size-full max-h-48')
-                ->attributes([
-                    'x-on:mouseover' => 'load($refs.video, manifest)',
-                    'x-on:mouseout' => 'unload()',
-                    'x-on:touchstart.passive' => 'load($refs.video, manifest)',
-                    'x-on:touchend.passive' => 'unload()',
+        html()
+            ->a()
+            ->link('videos.view', $video)
+            ->class('relative z-0 shrink-0 block size-full h-48 min-h-48 max-h-48')
+            ->attributes([
+                'x-cloak',
+                'x-show' => 'shown',
+                'x-on:mouseover.prevent' => 'load($refs.video, manifest)',
+                'x-on:mouseout.prevent' => 'unload()',
+                'x-on:touchstart.passive' => 'load($refs.video, manifest)',
+                'x-on:touchend.passive' => 'unload()',
+            ])
+            ->children([
+                html()
+                    ->img($video->thumbnail, $video->title)
+                    ->class('absolute inset-0 z-0 size-full min-h-48 max-h-48 rounded object-fill brightness-85')
+                    ->srcset($video->srcset)
+                    ->loading('lazy')
+                    ->crossorigin('use-credentials')
+                    ->attributes([
+                        'x-cloak',
+                        'x-show' => 'shown',
+                        'sizes' => '(min-width: 768px) 768px, 320px',
+                    ]),
+
+                html()
+                    ->element('video')
+                    ->ignore()
+                    ->class('absolute inset-0 z-10 size-full min-h-48 max-h-48 rounded object-fill brightness-95')
+                    ->attributes([
+                        'x-cloak',
+                        'x-ref' => 'video',
+                        'x-show' => 'ready',
+                        'playsinline',
+                        'autoplay',
+                        'muted',
+                        'loop',
+                    ]),
+
+                html()->div()->class('absolute z-30 inset-x-1.5 bottom-1.5 flex flex-col-reverse gap-y-0.5')->children([
+                    html()->element('h1')->text($video->title)->class('text-sm leading-none capitalize truncate'),
+
+                    html()->element('dl')->class('list text-xs text-secondary-300')
+                        ->childrenIf($video->duration, [
+                            html()->element('dt')->text('Time')->class('sr-only'),
+                            html()->element('dd')->text($video->timestamp)
+                        ])
+                        ->childrenIf($video->identifier, [
+                            html()->element('dt')->text('ID')->class('sr-only'),
+                            html()->element('dd')->text($video->identifier)
+                        ])
+                        ->childrenIf($video->caption, [
+                            html()->element('dt')->text('Caption')->class('sr-only'),
+                            html()->element('dd')->text('CC')
+                        ]),
                 ])
-                ->child(
-                    html()->div()->class('absolute inset-x-1.5 bottom-1.5 flex flex-col-reverse gap-y-0.5')->children([
-                        html()->element('h1')->text($video->title)->class('text-sm leading-none capitalize truncate'),
-                        html()->element('dl')->class('list text-xs text-secondary-300')
-                            ->childrenIf($video->duration, [
-                                html()->element('dt')->text('Time')->class('sr-only'),
-                                html()->element('dd')->text($video->timestamp)
-                            ])
-                            ->childrenIf($video->identifier, [
-                                html()->element('dt')->text('ID')->class('sr-only'),
-                                html()->element('dd')->text($video->identifier)
-                            ])
-                            ->childrenIf($video->caption, [
-                                html()->element('dt')->text('Caption')->class('sr-only'),
-                                html()->element('dd')->text('CC')
-                            ]),
-                    ])
-                ),
+            ]),
 
-            html()
-                ->img($video->thumbnail, $video->title)
-                ->srcset($video->srcset)
-                ->class('absolute inset-0 z-0 size-full max-h-48 rounded object-fill brightness-85')
-                ->loading('lazy')
-                ->crossorigin('use-credentials')
-                ->attributes([
-                    'x-cloak',
-                    'x-show' => 'shown',
-                    'sizes' => '(min-width: 768px) 768px, 320px',
-                ]),
-
-            html()
-                ->element('video')
-                ->ignore()
-                ->class('absolute inset-0 z-10 size-full max-h-48 rounded object-fill brightness-95')
-                ->attributes([
-                    'x-cloak',
-                    'x-ref' => 'video',
-                    'x-show' => 'shown && ready',
-                    'x-transition.opacity',
-                    'playsinline',
-                    'autoplay',
-                    'muted',
-                    'loop',
-                ]),
-        ]),
-
-        html()->div()->class('card-body py-1.5 dl justify-center')->children($video->tags->take(3), fn (Tag $tag) =>
-            html()
-                ->a()
-                ->link('tags.view', $tag)
-                ->text($tag->name)
-                ->class('text-sm text-secondary-400')
+        html()->div()->class('card-body py-1.5 dl justify-center')->children($video->tags->take(3), fn (Tag $tag) => html()
+            ->a()
+            ->link('tags.view', $tag)
+            ->text($tag->name)
+            ->class('text-sm text-secondary-400')
         ),
     ])
 }}
