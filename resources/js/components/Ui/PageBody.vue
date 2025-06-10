42417193d05b303402d8e4486d881aa32975979e
francoism90 <francoism90@users.noreply.github.com>
2025-06-10 20:32:48 +0000

    fix(ui): update PageBody component to increase maximum width for improved layout

--- resources/js/components/Ui/PageBody.vue
+++ resources/js/components/Ui/PageBody.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="max-w-xl flex-1 contain-inline-size contain-layout contain-paint contain-style"
+    class="max-w-2xl flex-1 contain-inline-size contain-layout contain-paint contain-style"
   >
     <slot />
   </div>
