e5fae4d78df4aeb597761f8bce6f0383ac1d2750
francoism90 <francoism90@users.noreply.github.com>
2025-06-13 19:14:52 +0000

    refactor: Remove unused NavBar import from VideoView component

--- resources/js/pages/Videos/VideoView.vue
+++ resources/js/pages/Videos/VideoView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import HistoryBack from '@/components/Actions/HistoryBack.vue'
-import NavBar from '@/components/Ui/NavBar.vue'
 import PageBody from '@/components/Ui/PageBody.vue'
 import PageCard from '@/components/Ui/PageCard.vue'
 import PageList from '@/components/Ui/PageList.vue'
