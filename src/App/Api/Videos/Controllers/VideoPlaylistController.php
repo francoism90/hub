43baf652103a8f96a043efb6a6bf2ba53d269810
francoism90 <francoism90@users.noreply.github.com>
2025-07-06 18:04:14 +0000

    fix: reduce cache response duration from 300 to 90 seconds in VideoPlaylistController

--- src/App/Api/Videos/Controllers/VideoPlaylistController.php
+++ src/App/Api/Videos/Controllers/VideoPlaylistController.php
@@ -19,7 +19,7 @@ class VideoPlaylistController extends Controller implements HasMiddleware
     {
         return [
             new Middleware('auth:sanctum'),
-            new Middleware('cache_response:300'),
+            new Middleware('cache_response:90'),
         ];
     }
 
