ea09ae31ab38307fe57319bdeeae084f9a7ac016
francoism90 <francoism90@users.noreply.github.com>
2025-07-07 09:09:10 +0000

    feat: implement broadcasting for UserHasBeenReleased event

--- src/Domain/Users/Events/UserHasBeenReleased.php
+++ src/Domain/Users/Events/UserHasBeenReleased.php
@@ -5,16 +5,34 @@ declare(strict_types=1);
 namespace Domain\Users\Events;
 
 use Domain\Users\Models\User;
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UserHasBeenReleased implements ShouldDispatchAfterCommit
+class UserHasBeenReleased implements ShouldBroadcast, ShouldDispatchAfterCommit
 {
     use Dispatchable;
     use InteractsWithSockets;
     use SerializesModels;
 
     public function __construct(public User $user) {}
+
+    public function broadcastOn(): Channel
+    {
+        return new PrivateChannel('video.'.$this->user->getRouteKey());
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'user.updated';
+    }
+
+    public function broadcastQueue(): string
+    {
+        return 'broadcasts';
+    }
 }
