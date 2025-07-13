6976a78ca5e17be5411bfb8c863f239695219ef0
francoism90 <francoism90@users.noreply.github.com>
2025-07-13 13:38:57 +0000

    refactor(VideoCard): update styles and layout for improved UI consistency

--- resources/js/components/Video/VideoCard.vue
+++ resources/js/components/Video/VideoCard.vue
@@ -17,32 +17,44 @@ const link = computed(() => show.url(props.item.id))
   <UCard
     variant="solid"
     :ui="{
-      root: 'relative h-52 min-h-52 rounded-2xl bg-gradient-to-t from-neutral-900 to-neutral-800',
+      root: 'relative h-60 min-h-60 rounded-2xl bg-transparent',
       body: 'absolute inset-0 z-0 !p-0',
-      footer: 'absolute inset-x-0 bottom-0 z-10 h-14 min-h-14 bg-neutral-200/10 !p-0 backdrop-blur-sm',
+      footer: 'absolute inset-x-0 bottom-0 z-20 h-14 min-h-14 bg-neutral-200/10 !p-0 backdrop-blur-sm',
     }"
   >
     <Link :href="link">
+      <div class="absolute inset-0 z-10 size-full bg-gradient-to-t from-neutral-800/50 to-transparent" />
+
       <img
         :srcset="item.srcset"
         :src="item.thumbnail"
         :alt="item.name"
-        class="h-52 w-full rounded-2xl object-fill"
+        class="h-60 w-full rounded-2xl object-fill"
+        loading="lazy"
       />
+
+      <div class="absolute inset-x-0 bottom-16 z-10">
+        <div class="flex flex-col px-4">
+          <span class="line-clamp-1 text-sm font-medium tracking-tight text-neutral-100">Spiderman: No way home</span>
+          <span class="line-clamp-1 text-xs font-light tracking-tight text-neutral-100">Dec 2021</span>
+        </div>
+      </div>
     </Link>
 
     <template #footer>
-      <div class="flex size-full items-center gap-2 px-4">
-        <div class="w-6">
-          <UButton
-            icon="i-hugeicons-play"
+      <div class="flex size-full items-center gap-3 px-3">
+        <div class="flex w-6 justify-start">
+          <UIcon
+            name="i-hugeicons-play"
             class="size-6 rounded-full bg-neutral-100/30 p-1 text-neutral-100"
           />
         </div>
 
         <div class="flex flex-1 flex-col justify-center">
-          <span class="line-clamp-1 text-sm leading-none font-medium tracking-tight text-neutral-100">Spiderman: No way home</span>
-          <span class="line-clamp-1 text-xs font-light tracking-tight text-neutral-100">Dec 2021</span>
+          <USlider
+            :default-value="0"
+            size="xs"
+          />
         </div>
 
         <div class="flex w-20 justify-end gap-3">
