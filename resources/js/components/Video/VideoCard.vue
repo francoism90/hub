dc111aa32e10853aa238eed7a0fcfaf80491a630
francoism90 <francoism90@users.noreply.github.com>
2025-07-15 11:45:48 +0000

    refactor(VideoCard): simplify layout and improve hover effects

--- resources/js/components/Video/VideoCard.vue
+++ resources/js/components/Video/VideoCard.vue
@@ -2,15 +2,13 @@
 import { show } from '@/actions/App/Web/Videos/Controllers/VideoController'
 import type { Video } from '@/types'
 import { Link } from '@inertiajs/vue3'
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 
 interface Props {
   item: Video
 }
 
 const props = defineProps<Props>()
-
-const hover = ref<boolean>(false)
 
 const link = computed(() => show.url(props.item.id))
 </script>
@@ -19,8 +17,8 @@ const link = computed(() => show.url(props.item.id))
   <UCard
     variant="solid"
     :ui="{
-      root: 'relative h-52 min-h-52 rounded-2xl bg-transparent',
-      body: 'absolute inset-0 z-0 !p-0',
+      root: 'group h-52 min-h-52 rounded-none bg-transparent',
+      body: 'relative !p-0',
     }"
   >
     <Link :href="link">
@@ -30,18 +28,28 @@ const link = computed(() => show.url(props.item.id))
         :srcset="item.srcset"
         :src="item.thumbnail"
         :alt="item.name"
-        class="h-52 w-full rounded-2xl object-fill"
+        class="h-52 w-full rounded-2xl object-fill group-hover:rounded-none"
         loading="lazy"
       />
 
-      <div class="absolute inset-x-4 bottom-6 z-10">
-        <div class="flex flex-col">
-          <h2 class="line-clamp-1 text-sm font-medium tracking-tight text-neutral-100">Spiderman: No way home</h2>
-          <p class="line-clamp-1 text-xs font-light tracking-tight text-neutral-100">
-            {{ item.timestamp }}
-          </p>
-        </div>
+      <div class="absolute inset-x-4 bottom-4 z-20">
+        <h2 class="line-clamp-1 text-sm font-medium tracking-tight text-neutral-100">Spiderman: No way home</h2>
+        <p class="line-clamp-1 text-xs font-light tracking-tight text-neutral-100">
+          {{ item.timestamp }}
+        </p>
       </div>
     </Link>
+
+    <div class="absolute inset-x-0 bottom-0 z-30 hidden group-hover:block">
+      <USlider
+        :default-value="50"
+        :ui="{
+          root: 'flex h-8 items-end',
+          track: 'h-1 rounded-none',
+          range: 'rounded-none',
+          thumb: 'hidden',
+        }"
+      />
+    </div>
   </UCard>
 </template>
